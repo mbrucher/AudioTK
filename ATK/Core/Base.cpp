11eb6b7080296db086ac42ca55c2a359f1106559
Matthieu Brucher <matthieu.brucher@gmail.com>
2013-12-14 00:07:44 +0000

    Starting implementation. Still need to figure out how the data will actually flow. Perhaps using multimethods/dispatchers?

--- ATK/Core/Base.cpp
+++ ATK/Core/Base.cpp
@@ -4,12 +4,32 @@
 
 #include "Base.h"
 
+#include <cstdint>
+
 namespace ATK
 {
+  template<typename DataType>
+  Base<DataType>::Base(int nbInputPorts, int nbOutputPorts)
+  :nbInputPorts(nbInputPorts), nbOutputPorts(nbOutputPorts), connections(nbInputPorts, NULL)
+  {
+  }
 
-Base::~Base()
-{
-}
+  template<typename DataType>
+  Base<DataType>::~Base()
+  {
+  }
 
+  template<typename DataType>
+  void Base<DataType>::setInputPort(int port, Base<DataType> *filter)
+  {
+    if(port >= 0 && port < nbInputPorts)
+      connections[port] = filter;
+  }
+  
+  template Base<int16_t>;
+  template Base<int32_t>;
+  template Base<int64_t>;
+  template Base<float>;
+  template Base<double>;
 }
 
--- ATK/Core/Base.h
+++ ATK/Core/Base.h
@@ -2,15 +2,27 @@
  * \file Base.h
  */
 
-#ifndef ATK_CORE_BASE
-#define ATK_CORE_BASE
+#ifndef ATK_CORE_BASE_H
+#define ATK_CORE_BASE_H
+
+#include <vector>
 
 namespace ATK
 {
+  template<typename DataType>
   class Base
   {
   public:
+    Base(int nbInputPorts, int nbOutputPorts);
     virtual ~Base();
+    
+    void setInputPort(int port, Base<DataType>* filter);
+    
+  protected:
+    void virtual process() = 0;
+    int nbInputPorts;
+    int nbOutputPorts;
+    std::vector<std::pair<int, Base<DataType> >* > connections;
   };
 }
 
