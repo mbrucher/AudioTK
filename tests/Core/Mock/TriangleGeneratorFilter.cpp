8d4152da7751491442f1f71c529fb2353e95cead
Matthieu Brucher <matthieu.brucher@gmail.com>
2013-12-17 23:23:40 +0000

    Triangle generator

--- tests/Core/Mock/TriangleGeneratorFilter.cpp
+++ tests/Core/Mock/TriangleGeneratorFilter.cpp
@@ -22,5 +22,21 @@ namespace ATK
       outputs[0].reset(new DataType[size]);
       outputs_size[0] = size;
     }
+    
+    int i = 0;
+    for(i = 0; i < std::min(size, output_sampling_rate - state); ++i)
+    {
+      outputs[0][i] = 100 * (state + i) * (ascending ? 1 : -1);
+    }
+    if(i == output_sampling_rate - state)
+    {
+      ascending = !ascending;
+      state = (ascending ? 1 : -1) * -output_sampling_rate;
+    }
+    int j = 0;
+    for(;i < size; ++i, ++j)
+    {
+      outputs[0][i] = 100 * (state + j) * (ascending ? 1 : -1);
+    }
   }
 }
