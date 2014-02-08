2d3eaa93b75c9f31de9cbe10ebb67eb45255321a
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-02-08 17:37:49 +0000

    Started EQ tests. Something strange with the band pass filter.

---
+++ tests/EQ/SecondOrderFilter.cpp
@@ -0,0 +1,71 @@
+/**
+ * \ file SecondOrder.cpp
+ */
+
+#include <ATK/EQ/SecondOrderFilter.h>
+
+#include <ATK/Mock/FFTCheckerFilter.h>
+#include <ATK/Mock/SinusGeneratorFilter.h>
+
+#define BOOST_TEST_NO_MAIN
+#include <boost/test/unit_test.hpp>
+
+#define PROCESSSIZE (1024*1024)
+
+BOOST_AUTO_TEST_CASE( SecondOrderFilter_BandPassCoefficients_1k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(1000);
+  
+  ATK::SecondOrderFilter<ATK::BandPassCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_Q(1);
+  filter.set_cut_frequency(100);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0.31610222820014583));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  
+  filter.process(1024*64);
+  
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( SecondOrderFilter_BandPassCoefficients_100_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(100);
+  
+  ATK::SecondOrderFilter<ATK::BandPassCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_Q(1);
+  filter.set_cut_frequency(100);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  //frequency_checks.push_back(std::make_pair(10, 0));
+  //frequency_checks.push_back(std::make_pair(100, 0.8399495266820497));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  
+  filter.process(1024*64);
+  
+  checker.process(PROCESSSIZE);
+}
