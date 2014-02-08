09fbbb304cc8cc154e3f44a5a798952fa8a90b6c
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-02-08 17:57:17 +0000

    Added new tests. Close enough to the usual approximations.

--- tests/EQ/SecondOrderFilter.cpp
+++ tests/EQ/SecondOrderFilter.cpp
@@ -70,6 +70,35 @@ BOOST_AUTO_TEST_CASE( SecondOrderFilter_BandPassCoefficients_100_test )
   checker.process(PROCESSSIZE);
 }
 
+BOOST_AUTO_TEST_CASE( SecondOrderFilter_BandPassCoefficients_2k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(2000);
+  
+  ATK::SecondOrderFilter<ATK::BandPassCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_Q(1);
+  filter.set_cut_frequency(100);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(2000, 0.22326595903140814));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  
+  filter.process(1024*64);
+  
+  checker.process(PROCESSSIZE);
+}
+
 BOOST_AUTO_TEST_CASE( SecondOrderFilter_LowPassCoefficients_1k_test )
 {
   ATK::SinusGeneratorFilter<double> generator;
@@ -116,6 +145,34 @@ BOOST_AUTO_TEST_CASE( SecondOrderFilter_LowPassCoefficients_100_test )
   frequency_checks.push_back(std::make_pair(10, 0));
   frequency_checks.push_back(std::make_pair(100, 0.8399580865576376));
   frequency_checks.push_back(std::make_pair(1000, 0));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  
+  filter.process(1024*64);
+  
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( SecondOrderFilter_LowPassCoefficients_2k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(2000);
+  
+  ATK::SecondOrderFilter<ATK::LowPassCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_cut_frequency(100);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(2000, 0.04984673793807906));
   checker.set_checks(frequency_checks);
   
   checker.set_input_port(0, &filter, 0);
