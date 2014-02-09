312059cf524bc257eda4db3766671849c34b1cbd
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-02-09 11:41:52 +0000

    High pass tests

--- tests/EQ/SecondOrderFilter.cpp
+++ tests/EQ/SecondOrderFilter.cpp
@@ -182,3 +182,115 @@ BOOST_AUTO_TEST_CASE( SecondOrderFilter_LowPassCoefficients_2k_test )
   
   checker.process(PROCESSSIZE);
 }
+
+BOOST_AUTO_TEST_CASE( SecondOrderFilter_LowPassCoefficients_200_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(200);
+  
+  ATK::SecondOrderFilter<ATK::LowPassCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_cut_frequency(100);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(200, 0.49220974122133615));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  
+  filter.process(1024*64);
+  
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( SecondOrderFilter_HighPassCoefficients_1k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(1000);
+  
+  ATK::SecondOrderFilter<ATK::HighPassCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_cut_frequency(1000);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0.8697981291708585));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  
+  filter.process(1024*64);
+  
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( SecondOrderFilter_HighPassCoefficients_10k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(10000);
+  
+  ATK::SecondOrderFilter<ATK::HighPassCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_cut_frequency(1000);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(10000, 1.0344582218093583));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  
+  filter.process(1024*64);
+  
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( SecondOrderFilter_HighPassCoefficients_500_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(500);
+  
+  ATK::SecondOrderFilter<ATK::HighPassCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_cut_frequency(1000);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(500, 0.5091584471108357));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  
+  filter.process(1024*64);
+  
+  checker.process(PROCESSSIZE);
+}
