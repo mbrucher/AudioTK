2a05a6db08688a70415f67e92f419d7df62759b4
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-02-23 22:20:34 +0000

    Added another test case. Need to add datatype conversion + resampling

--- tests/IO/InWavFilter.cpp
+++ tests/IO/InWavFilter.cpp
@@ -16,7 +16,7 @@
 
 #define PROCESSSIZE (1024*1024)
 
-BOOST_AUTO_TEST_CASE( InWavFilter_In_test )
+BOOST_AUTO_TEST_CASE( InWavFilter_In_1k_test )
 {
   ATK::SinusGeneratorFilter<double> generator;
   generator.set_output_sampling_rate(48000);
@@ -42,3 +42,41 @@ BOOST_AUTO_TEST_CASE( InWavFilter_In_test )
   checker.process(PROCESSSIZE);
 }
 
+BOOST_AUTO_TEST_CASE( InWavFilter_In_1k2k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator1k;
+  generator1k.set_output_sampling_rate(48000);
+  generator1k.set_amplitude(-1);
+  generator1k.set_frequency(1000);
+  ATK::SinusGeneratorFilter<double> generator2k;
+  generator2k.set_output_sampling_rate(48000);
+  generator2k.set_amplitude(-1);
+  generator2k.set_frequency(2000);
+  
+  ATK::InWavFilter<double> filter(ATK_SOURCE_TREE "/tests/data/sinus1k2k.wav");
+  filter.set_output_sampling_rate(48000);
+  
+  ATK::SumFilter<double> sumfilter1;
+  sumfilter1.set_input_sampling_rate(48000);
+  sumfilter1.set_input_port(0, &generator1k, 0);
+  sumfilter1.set_input_port(1, &filter, 0);
+
+  ATK::SumFilter<double> sumfilter2;
+  sumfilter2.set_input_sampling_rate(48000);
+  sumfilter2.set_input_port(0, &generator2k, 0);
+  sumfilter2.set_input_port(1, &filter, 1);
+  
+  ATK::SumFilter<double> sumfilter3;
+  sumfilter3.set_input_sampling_rate(48000);
+  sumfilter3.set_input_port(0, &sumfilter1, 0);
+  sumfilter3.set_input_port(1, &sumfilter2, 0);
+
+  ATK::TriangleCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(48000);
+  checker.set_amplitude(0);
+  checker.set_frequency(1000);
+  
+  checker.set_input_port(0, &sumfilter3, 0);
+  
+  checker.process(PROCESSSIZE);
+}
