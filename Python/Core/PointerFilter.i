f664477c4c124baf3f779ef380be01ffa8b8e748
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-03-25 23:15:01 +0000

    Starting conversion tests

--- Python/Core/PointerFilter.i
+++ Python/Core/PointerFilter.i
@@ -4,19 +4,25 @@
 #include <ATK/Core/InPointerFilter.h>
 %}
 
+%apply (int* INPLACE_ARRAY2, int DIM1, int DIM2) {(int* array, long size, int channels)}
+%apply (long long* INPLACE_ARRAY2, int DIM1, int DIM2) {(long long* array, long size, int channels)}
+%apply (float* INPLACE_ARRAY2, int DIM1, int DIM2) {(float* array, long size, int channels)}
+%apply (double* INPLACE_ARRAY2, int DIM1, int DIM2) {(double* array, long size, int channels)}
+
 namespace ATK
 {
-template<class DataType>
-class InPointerFilter
-{
-public:
-  InPointerFilter(const DataType* array, long size, int channels, bool interleaved);
-  ~InPointerFilter();
-  void process(long size);
-};
+  template<class DataType>
+  class InPointerFilter
+  {
+  public:
+    InPointerFilter(DataType* array, long size, int channels, bool interleaved);
+    ~InPointerFilter();
+    void process(long size);
+    int get_nb_output_ports();
+  };
 }
 
-
-%apply (int* ARGOUT_ARRAY2, int DIM1, int DIM2) {(const int* array, long size, int channels)}
-
-%template(Int32InPointerFilter) ATK::InPointerFilter<std::int32_t>;
+%template(Int32InPointerFilter) ATK::InPointerFilter<int>;
+%template(Int64InPointerFilter) ATK::InPointerFilter<long long>;
+%template(FloatInPointerFilter) ATK::InPointerFilter<float>;
+%template(DoubleInPointerFilter) ATK::InPointerFilter<double>;
--- tests/Python/Core/PyATKCore_test.py
+++ tests/Python/Core/PyATKCore_test.py
@@ -1,5 +1,63 @@
 #!/usr/bin/env python
 
-def Int32PointerFilter_test():
-  from ATK.Core import Int32InPointerFilter
+from nose.tools import raises
 
+def Int32PointerFilter_new_test():
+  import numpy as np
+  from ATK.Core import Int32InPointerFilter
+  d = np.arange(1000, dtype=np.int32)
+  filter = Int32InPointerFilter(np.atleast_2d(d), False)
+  assert filter.get_nb_output_ports() == 1
+
+def Int64PointerFilter_new_test():
+  import numpy as np
+  from ATK.Core import Int64InPointerFilter
+  d = np.arange(1000, dtype=np.int64)
+  filter = Int64InPointerFilter(np.atleast_2d(d), False)
+  assert filter.get_nb_output_ports() == 1
+
+def FloatInPointerFilter_new_test():
+  import numpy as np
+  from ATK.Core import FloatInPointerFilter
+  d = np.arange(1000, dtype=np.float32)
+  filter = FloatInPointerFilter(np.atleast_2d(d), False)
+  assert filter.get_nb_output_ports() == 1
+
+def DoubleInPointerFilter_new_test():
+  import numpy as np
+  from ATK.Core import DoubleInPointerFilter
+  d = np.arange(1000, dtype=np.float64)
+  filter = DoubleInPointerFilter(np.atleast_2d(d), False)
+  assert filter.get_nb_output_ports() == 1
+
+@raises(TypeError)
+def Int32PointerFilter_new_fail_test():
+  import numpy as np
+  from ATK.Core import Int32InPointerFilter
+  d = np.arange(1000, dtype=np.int64)
+  filter = Int32InPointerFilter(np.atleast_2d(d), False)
+  assert filter.get_nb_output_ports() == 1
+
+@raises(TypeError)
+def Int64PointerFilter_new_fail_test():
+  import numpy as np
+  from ATK.Core import Int64InPointerFilter
+  d = np.arange(1000, dtype=np.float32)
+  filter = Int64InPointerFilter(np.atleast_2d(d), False)
+  assert filter.get_nb_output_ports() == 1
+
+@raises(TypeError)
+def FloatInPointerFilter_new_fail_test():
+  import numpy as np
+  from ATK.Core import FloatInPointerFilter
+  d = np.arange(1000, dtype=np.float64)
+  filter = FloatInPointerFilter(np.atleast_2d(d), False)
+  assert filter.get_nb_output_ports() == 1
+
+@raises(TypeError)
+def DoubleInPointerFilter_new_fail_test():
+  import numpy as np
+  from ATK.Core import DoubleInPointerFilter
+  d = np.arange(1000, dtype=np.int32)
+  filter = DoubleInPointerFilter(np.atleast_2d(d), False)
+  assert filter.get_nb_output_ports() == 1
