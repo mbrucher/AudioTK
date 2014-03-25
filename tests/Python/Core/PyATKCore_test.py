f67509812c03f9780876c10bc8b3f16f587445bb
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-03-25 23:23:46 +0000

    Data is now contiguous, but the matrix doesn't have the proper size

--- tests/Python/Core/PyATKCore_test.py
+++ tests/Python/Core/PyATKCore_test.py
@@ -5,59 +5,56 @@ from nose.tools import raises
 def Int32PointerFilter_new_test():
   import numpy as np
   from ATK.Core import Int32InPointerFilter
-  d = np.arange(1000, dtype=np.int32)
-  filter = Int32InPointerFilter(np.atleast_2d(d), False)
+  d = np.ascontiguousarray(np.arange(1000, dtype=np.int32)[None,:])
+  print d.shape
+  filter = Int32InPointerFilter(d, False)
   assert filter.get_nb_output_ports() == 1
 
 def Int64PointerFilter_new_test():
   import numpy as np
   from ATK.Core import Int64InPointerFilter
-  d = np.arange(1000, dtype=np.int64)
-  filter = Int64InPointerFilter(np.atleast_2d(d), False)
+  d = np.ascontiguousarray(np.arange(1000, dtype=np.int64)[None,:])
+  filter = Int64InPointerFilter(d, False)
   assert filter.get_nb_output_ports() == 1
 
 def FloatInPointerFilter_new_test():
   import numpy as np
   from ATK.Core import FloatInPointerFilter
-  d = np.arange(1000, dtype=np.float32)
-  filter = FloatInPointerFilter(np.atleast_2d(d), False)
+  d = np.ascontiguousarray(np.arange(1000, dtype=np.float32)[None,:])
+  filter = FloatInPointerFilter(d, False)
   assert filter.get_nb_output_ports() == 1
 
 def DoubleInPointerFilter_new_test():
   import numpy as np
   from ATK.Core import DoubleInPointerFilter
-  d = np.arange(1000, dtype=np.float64)
-  filter = DoubleInPointerFilter(np.atleast_2d(d), False)
+  d = np.ascontiguousarray(np.arange(1000, dtype=np.float64)[None,:])
+  filter = DoubleInPointerFilter(d, False)
   assert filter.get_nb_output_ports() == 1
 
 @raises(TypeError)
 def Int32PointerFilter_new_fail_test():
   import numpy as np
   from ATK.Core import Int32InPointerFilter
-  d = np.arange(1000, dtype=np.int64)
-  filter = Int32InPointerFilter(np.atleast_2d(d), False)
-  assert filter.get_nb_output_ports() == 1
+  d = np.ascontiguousarray(np.arange(1000, dtype=np.int64)[None,:])
+  filter = Int32InPointerFilter(d, False)
 
 @raises(TypeError)
 def Int64PointerFilter_new_fail_test():
   import numpy as np
   from ATK.Core import Int64InPointerFilter
-  d = np.arange(1000, dtype=np.float32)
-  filter = Int64InPointerFilter(np.atleast_2d(d), False)
-  assert filter.get_nb_output_ports() == 1
+  d = np.ascontiguousarray(np.arange(1000, dtype=np.float32)[None,:])
+  filter = Int64InPointerFilter(d, False)
 
 @raises(TypeError)
 def FloatInPointerFilter_new_fail_test():
   import numpy as np
   from ATK.Core import FloatInPointerFilter
-  d = np.arange(1000, dtype=np.float64)
-  filter = FloatInPointerFilter(np.atleast_2d(d), False)
-  assert filter.get_nb_output_ports() == 1
+  d = np.ascontiguousarray(np.arange(1000, dtype=np.float64)[None,:])
+  filter = FloatInPointerFilter(d, False)
 
 @raises(TypeError)
 def DoubleInPointerFilter_new_fail_test():
   import numpy as np
   from ATK.Core import DoubleInPointerFilter
-  d = np.arange(1000, dtype=np.int32)
-  filter = DoubleInPointerFilter(np.atleast_2d(d), False)
-  assert filter.get_nb_output_ports() == 1
+  d = np.ascontiguousarray(np.arange(1000, dtype=np.int32)[None,:])
+  filter = DoubleInPointerFilter(d, False)
