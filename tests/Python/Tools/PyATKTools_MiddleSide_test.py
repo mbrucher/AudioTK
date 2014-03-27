65027bf2d7f31fbb89cb6c9812cab549ea004afd
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-03-27 21:24:47 +0000

    MS Filter Python test

---
+++ tests/Python/Tools/PyATKTools_MiddleSide_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from nose.tools import raises
+
+def Decimation_test():
+  import numpy as np
+  from ATK.Core import DoubleInPointerFilter, DoubleOutPointerFilter
+  from ATK.Tools import DoubleMiddleSideFilter
+
+  from numpy.testing import assert_almost_equal
+  
+  t = np.arange(1000, dtype=np.float64)
+  input = np.sin(np.array((t, t)) * 1000 * 2 * np.pi / 48000)
+  output = np.ascontiguousarray(np.zeros(2000, dtype=np.float64).reshape(2, -1))
+
+  inputfilter = DoubleInPointerFilter(input, False)
+  msfilter = DoubleMiddleSideFilter()
+  outputfilter = DoubleOutPointerFilter(output, False)
+
+  inputfilter.set_output_sampling_rate(48000)
+  msfilter.set_input_sampling_rate(48000)
+  outputfilter.set_input_sampling_rate(48000)
+
+  msfilter.set_input_port(0, inputfilter, 0)
+  msfilter.set_input_port(1, inputfilter, 1)
+  outputfilter.set_input_port(0, msfilter, 0)
+  outputfilter.set_input_port(1, msfilter, 1)
+
+  outputfilter.process(1000)
+
+  assert_almost_equal(input[0], output[0])
+  assert_almost_equal(0, output[1])
+
