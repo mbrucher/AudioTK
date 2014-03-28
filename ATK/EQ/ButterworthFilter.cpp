1729ba0713b7831e3bb6c168b69c63916411016b
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-03-28 22:04:03 +0100

    Seting up interface

--- ATK/EQ/ButterworthFilter.cpp
+++ ATK/EQ/ButterworthFilter.cpp
@@ -5,3 +5,36 @@
 #include "ButterworthFilter.h"
 #include "IIRFilter.h"
 
+namespace ATK
+{
+  template <typename DataType>
+  ButterworthLowPassCoefficients<DataType>::ButterworthLowPassCoefficients()
+  :Parent(1, 1)
+  {
+  }
+  
+  template <typename DataType>
+  void ButterworthLowPassCoefficients<DataType>::set_cut_frequency(DataType cut_frequency)
+  {
+    this->cut_frequency = cut_frequency;
+    setup();
+  }
+
+  template <typename DataType>
+  typename ButterworthLowPassCoefficients<DataType>::DataType ButterworthLowPassCoefficients<DataType>::get_cut_frequency() const
+  {
+    return cut_frequency;
+  }
+  
+  template <typename DataType>
+  void ButterworthLowPassCoefficients<DataType>::setup()
+  {
+  }
+
+  template class ButterworthLowPassCoefficients<float>;
+  template class ButterworthLowPassCoefficients<double>;
+  
+  template class IIRFilter<ButterworthLowPassCoefficients<float> >;
+  template class IIRFilter<ButterworthLowPassCoefficients<double> >;
+
+}
--- ATK/EQ/ButterworthFilter.h
+++ ATK/EQ/ButterworthFilter.h
@@ -9,6 +9,30 @@
 
 namespace ATK
 {
+  template<typename DataType_>
+  class ButterworthLowPassCoefficients: public TypedBaseFilter<DataType_>
+  {
+  public:
+    typedef TypedBaseFilter<DataType_> Parent;
+    using typename Parent::DataType;
+    using Parent::setup;
+  protected:
+    DataType cut_frequency;
+    
+    const static int in_order=2;
+    const static int out_order=2;
+    
+    void setup();
+    
+    DataType coefficients_in[3];
+    DataType coefficients_out[2];
+    
+  public:
+    ButterworthLowPassCoefficients();
+    
+    void set_cut_frequency(DataType cut_frequency);
+    DataType get_cut_frequency() const;
+  };
 }
 
 #endif
