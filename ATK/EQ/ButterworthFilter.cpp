e1b67de63b3429bb5841a27d1a084b9d9a062697
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-04-05 23:12:28 +0100

    Refactored how butter worth coefficients are computed. Should put the common pieces somewhere else like IIR

--- ATK/EQ/ButterworthFilter.cpp
+++ ATK/EQ/ButterworthFilter.cpp
@@ -12,42 +12,129 @@
 namespace
 {
   template<typename DataType>
+  void create_butterworth_analog_coefficients(int order, std::vector<std::complex<DataType> >& z, std::vector<std::complex<DataType> >& p, DataType& k)
+  {
+    k = 1;
+    z.clear(); // no zeros for this filter type
+    p.clear();
+    for(int i = -order+1; i < order; i += 2)
+    {
+      p.push_back(std::complex<DataType>(-std::cos(boost::math::constants::pi<DataType>() * i / (2 * order)), -std::sin(boost::math::constants::pi<DataType>() * i / (2 * order))));
+    }
+  }
+
+  /// Transform the Wn=1 low pass analog filter in a Wn=Wn low pass filter
+  template<typename DataType>
+  void zpk_lp2lp(DataType Wn, std::vector<std::complex<DataType> >& z, std::vector<std::complex<DataType> >& p, DataType& k)
+  {
+    int relative_degree = p.size() - z.size();
+    
+    for(int i = 0; i < z.size(); ++i)
+    {
+      z[i] *= Wn;
+    }
+    for(int i = 0; i < p.size(); ++i)
+    {
+      p[i] *= Wn;
+    }
+    
+    k *= std::pow(Wn, relative_degree);
+  }
+  
+  /// Apply a bilinear transform on z, p, k
+  template<typename DataType>
+  void zpk_bilinear(int fs, std::vector<std::complex<DataType> >& z, std::vector<std::complex<DataType> >& p, DataType& k)
+  {
+    DataType fs2 = 2*fs;
+    
+    std::complex<DataType> f = 1;
+    for(int i = 0; i < z.size(); ++i)
+    {
+      f *= fs2 - z[i];
+    }
+    for(int i = 0; i < p.size(); ++i)
+    {
+      f /= fs2 - p[i];
+    }
+    k *= f.real();
+
+    for(int i = 0; i < z.size(); ++i)
+    {
+      z[i] = (fs2 + z[i]) / (fs2 - z[i]);
+    }
+    for(int i = 0; i < p.size(); ++i)
+    {
+      p[i] = (fs2 + p[i]) / (fs2 - p[i]);
+    }
+
+    z.resize(p.size(), -1);
+  }
+
+  /// Transforms the z, p, k coefficients in b, a form
+  template<typename DataType>
+  void zpk2ba(int fs, const std::vector<std::complex<DataType> >& z, const std::vector<std::complex<DataType> >& p, DataType k, boost::math::tools::polynomial<DataType>& b, boost::math::tools::polynomial<DataType>& a)
+  {
+    b = boost::math::tools::polynomial<DataType>(k);
+    
+    for(int i = 0; i < z.size(); ++i)
+    {
+      if(z[i].imag() == 0)
+      {
+        DataType temp[2] = {-z[i].real(), 1};
+        boost::math::tools::polynomial<DataType> poly1(temp, 1);
+        b *= poly1;
+      }
+      else if(z[i].imag() < 0)
+      {
+        DataType temp[3] = {z[i].real() * z[i].real() + z[i].imag() * z[i].imag(), -2 * z[i].real(), 1};
+        boost::math::tools::polynomial<DataType> poly2(temp, 2);
+        b *= poly2;
+      }
+    }
+
+    a = boost::math::tools::polynomial<DataType>(1);
+    for(int i = 0; i < p.size(); ++i)
+    {
+      if(p[i].imag() == 0)
+      {
+        DataType temp[2] = {-p[i].real(), 1};
+        boost::math::tools::polynomial<DataType> poly1(temp, 1);
+        a *= poly1;
+      }
+      else if (p[i].imag() < 0)
+      {
+        DataType temp[3] = {p[i].real() * p[i].real() + p[i].imag() * p[i].imag(), -2 * p[i].real(), 1};
+        boost::math::tools::polynomial<DataType> poly2(temp, 2);
+        a *= poly2;
+      }
+    }
+  }
+  
+  template<typename DataType>
   void create_default_coeffs(int order, DataType Wn, std::vector<DataType>& coefficients_in, std::vector<DataType>& coefficients_out)
   {
-    DataType coeff = 1 / Wn;
-    DataType temp1[2] = {1,-1};
-    DataType temp2[2] = {1,+1};
-    boost::math::tools::polynomial<DataType> poly1(temp1, 1);
-    boost::math::tools::polynomial<DataType> poly2(temp2, 1);
+    std::vector<std::complex<DataType> > z;
+    std::vector<std::complex<DataType> > p;
+    DataType k;
     
-    for (int i = 0; i < order+1; ++i)
+    int fs = 2;
+    create_butterworth_analog_coefficients(order, z, p, k);
+    DataType warped = 2 * fs * std::tan(boost::math::constants::pi<DataType>() *  Wn / fs);
+    zpk_lp2lp(warped, z, p, k);
+    zpk_bilinear(fs, z, p, k);
+    
+    boost::math::tools::polynomial<DataType> b;
+    boost::math::tools::polynomial<DataType> a;
+    
+    zpk2ba(fs, z, p, k, b, a);
+    
+    for(int i = 0; i < order + 1; ++i)
     {
-      coefficients_in[i] = boost::math::binomial_coefficient<DataType>(order, i);
+      coefficients_in[i] = b[i];
     }
-    
-    boost::math::tools::polynomial<DataType> poly(1);
-    
-    if(order % 2 != 0)
+    for(int i = 0; i < order; ++i)
     {
-      poly = poly2 + coeff * poly1;
-    }
-    
-    for(int i = 1; i <= order/2; ++i)
-    {
-      boost::math::tools::polynomial<DataType> polytemp = coeff * coeff * poly1 * poly1;
-      polytemp -= 2 * coeff * std::cos((2. * i + order - 1) / (2 * order) * boost::math::constants::pi<DataType>()) * poly1 * poly2;
-      polytemp += poly2 * poly2;
-      
-      poly *= polytemp;
-    }
-    
-    for (int i = 1; i < poly.size(); ++i)
-    {
-      coefficients_out[order - i] = - poly[i] / poly[0];
-    }
-    for (int i = 0; i < order+1; ++i)
-    {
-      coefficients_in[order - i] /= poly[0];
+      coefficients_out[i] = a[i];
     }
   }
 }
@@ -87,7 +174,7 @@ namespace ATK
     coefficients_in.assign(in_order+1, 0);
     coefficients_out.assign(out_order, 0);
     
-    create_default_coeffs(in_order, (boost::math::constants::pi<DataType>() * cut_frequency) / input_sampling_rate, coefficients_in, coefficients_out);
+    create_default_coeffs(in_order, 2 * DataType(cut_frequency) / input_sampling_rate, coefficients_in, coefficients_out);
   }
 
   template <typename DataType>
@@ -123,12 +210,12 @@ namespace ATK
     coefficients_in.assign(in_order+1, 0);
     coefficients_out.assign(out_order, 0);
     
-    create_default_coeffs(in_order, 2 * boost::math::constants::pi<DataType>() * (1 - DataType(cut_frequency) / (input_sampling_rate / 2)), coefficients_in, coefficients_out);
-    /*for(int i = in_order - 1; i >= 0; i -= 2)
+    create_default_coeffs(in_order, (input_sampling_rate - 2 * DataType(cut_frequency)) / input_sampling_rate, coefficients_in, coefficients_out);
+    for(int i = in_order - 1; i >= 0; i -= 2)
     {
       coefficients_in[i] = - coefficients_in[i];
       coefficients_out[i] = - coefficients_out[i];
-    }*/
+    }
   }
 
   template class ButterworthLowPassCoefficients<float>;
