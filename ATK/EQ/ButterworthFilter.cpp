ca49e34d3a340d0271962be492435dccf99abb14
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-04-05 23:16:04 +0100

    Put zpk common functions inside IIR

--- ATK/EQ/ButterworthFilter.cpp
+++ ATK/EQ/ButterworthFilter.cpp
@@ -2,7 +2,6 @@
  * \file ButterworthFilter.cpp
  */
 
-#include <boost/math/special_functions/binomial.hpp>
 #include <boost/math/constants/constants.hpp>
 #include <boost/math/tools/polynomial.hpp>
 
@@ -20,93 +19,6 @@ namespace
     for(int i = -order+1; i < order; i += 2)
     {
       p.push_back(std::complex<DataType>(-std::cos(boost::math::constants::pi<DataType>() * i / (2 * order)), -std::sin(boost::math::constants::pi<DataType>() * i / (2 * order))));
-    }
-  }
-
-  /// Transform the Wn=1 low pass analog filter in a Wn=Wn low pass filter
-  template<typename DataType>
-  void zpk_lp2lp(DataType Wn, std::vector<std::complex<DataType> >& z, std::vector<std::complex<DataType> >& p, DataType& k)
-  {
-    int relative_degree = p.size() - z.size();
-    
-    for(int i = 0; i < z.size(); ++i)
-    {
-      z[i] *= Wn;
-    }
-    for(int i = 0; i < p.size(); ++i)
-    {
-      p[i] *= Wn;
-    }
-    
-    k *= std::pow(Wn, relative_degree);
-  }
-  
-  /// Apply a bilinear transform on z, p, k
-  template<typename DataType>
-  void zpk_bilinear(int fs, std::vector<std::complex<DataType> >& z, std::vector<std::complex<DataType> >& p, DataType& k)
-  {
-    DataType fs2 = 2*fs;
-    
-    std::complex<DataType> f = 1;
-    for(int i = 0; i < z.size(); ++i)
-    {
-      f *= fs2 - z[i];
-    }
-    for(int i = 0; i < p.size(); ++i)
-    {
-      f /= fs2 - p[i];
-    }
-    k *= f.real();
-
-    for(int i = 0; i < z.size(); ++i)
-    {
-      z[i] = (fs2 + z[i]) / (fs2 - z[i]);
-    }
-    for(int i = 0; i < p.size(); ++i)
-    {
-      p[i] = (fs2 + p[i]) / (fs2 - p[i]);
-    }
-
-    z.resize(p.size(), -1);
-  }
-
-  /// Transforms the z, p, k coefficients in b, a form
-  template<typename DataType>
-  void zpk2ba(int fs, const std::vector<std::complex<DataType> >& z, const std::vector<std::complex<DataType> >& p, DataType k, boost::math::tools::polynomial<DataType>& b, boost::math::tools::polynomial<DataType>& a)
-  {
-    b = boost::math::tools::polynomial<DataType>(k);
-    
-    for(int i = 0; i < z.size(); ++i)
-    {
-      if(z[i].imag() == 0)
-      {
-        DataType temp[2] = {-z[i].real(), 1};
-        boost::math::tools::polynomial<DataType> poly1(temp, 1);
-        b *= poly1;
-      }
-      else if(z[i].imag() < 0)
-      {
-        DataType temp[3] = {z[i].real() * z[i].real() + z[i].imag() * z[i].imag(), -2 * z[i].real(), 1};
-        boost::math::tools::polynomial<DataType> poly2(temp, 2);
-        b *= poly2;
-      }
-    }
-
-    a = boost::math::tools::polynomial<DataType>(1);
-    for(int i = 0; i < p.size(); ++i)
-    {
-      if(p[i].imag() == 0)
-      {
-        DataType temp[2] = {-p[i].real(), 1};
-        boost::math::tools::polynomial<DataType> poly1(temp, 1);
-        a *= poly1;
-      }
-      else if (p[i].imag() < 0)
-      {
-        DataType temp[3] = {p[i].real() * p[i].real() + p[i].imag() * p[i].imag(), -2 * p[i].real(), 1};
-        boost::math::tools::polynomial<DataType> poly2(temp, 2);
-        a *= poly2;
-      }
     }
   }
   
