9a0849fd8b8640623c44bcefd6946e243ef90c54
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-06-01 23:27:34 +0200

    Starting SD1 Tone filter, as an IIR filter

---
+++ ATK/EQ/SD1ToneFilter.cpp
@@ -0,0 +1,70 @@
+/**
+ * \file SD1ToneFilter.cpp
+ */
+
+#include "SD1ToneFilter.h"
+#include "IIRFilter.h"
+
+namespace ATK
+{
+  template<typename DataType>
+  SD1ToneCoefficients<DataType>::SD1ToneCoefficients()
+  :TypedBaseFilter<DataType>(1, 1), R1(1000), R2(22000), R3(470), R4(10000),
+    C1(0.000000018), C2(0.000000027), C3(0.00000001), alpha(1), in_order(2), out_order(2)
+  {
+  }
+
+  template<typename DataType>
+  void SD1ToneCoefficients<DataType>::setup()
+  {
+    Parent::setup();
+    
+    coefficients_in.assign(in_order+1, 0);
+    coefficients_out.assign(out_order, 0);
+
+    DataType tempm[2] = {2 * input_sampling_rate, -2 * input_sampling_rate};
+    DataType tempp[2] = {1, 1};
+    boost::math::tools::polynomial<DataType> poly1(tempm, 1);
+    boost::math::tools::polynomial<DataType> poly2(tempp, 1);
+
+    boost::math::tools::polynomial<DataType> b;
+    boost::math::tools::polynomial<DataType> a;
+    
+    b += poly2 * poly2;
+    b += poly2 * poly1 * (C2*R3+R4*C3-alpha*(1-alpha)*R2*C2-alpha*C2*R4);
+    b += poly1 * poly1 * (C3*R4*(R3*C2-alpha*(1-alpha)*R2*C2));
+
+    a += poly2 * poly2;
+    a += poly2 * poly1 * (C2*R3+R1*C1-alpha*(1-alpha)*R2*C2-(1-alpha)*C2*R1);
+    a += poly1 * poly1 * (C1*R1*(R3*C2-alpha*(1-alpha)*R2*C2));
+
+    for(int i = 0; i < in_order + 1; ++i)
+    {
+      coefficients_in[i] = b[i];
+    }
+    for(int i = 0; i < out_order; ++i)
+    {
+      coefficients_out[i] = -a[i];
+    }
+  }
+
+  template<typename DataType>
+  void SD1ToneCoefficients<DataType>::set_tone(DataType alpha)
+  {
+    this->alpha = alpha;
+
+    setup();
+  }
+  
+  template<typename DataType>
+  typename SD1ToneCoefficients<DataType>::DataType SD1ToneCoefficients<DataType>::get_tone() const
+  {
+    return alpha;
+  }
+
+  template class SD1ToneCoefficients<float>;
+  template class SD1ToneCoefficients<double>;
+  
+  template class IIRFilter<SD1ToneCoefficients<float> >;
+  template class IIRFilter<SD1ToneCoefficients<double> >;
+}
---
+++ ATK/EQ/SD1ToneFilter.h
@@ -0,0 +1,38 @@
+/**
+ * \file SD1ToneFilter.h
+ */
+
+#ifndef ATK_EQ_SD1TONEFILTERFILTER_H
+#define ATK_EQ_SD1TONEFILTERFILTER_H
+
+#include <ATK/Core/TypedBaseFilter.h>
+
+namespace ATK
+{
+  template<typename DataType_>
+  class SD1ToneCoefficients: public TypedBaseFilter<DataType_>
+  {
+  public:
+    typedef TypedBaseFilter<DataType_> Parent;
+    using typename Parent::DataType;
+    using Parent::input_sampling_rate;
+  protected:
+    DataType R1, R2, R3, R4, C1, C2, C3, alpha;
+    
+    int in_order;
+    int out_order;
+    
+    void setup();
+    
+    std::vector<DataType> coefficients_in;
+    std::vector<DataType> coefficients_out;
+    
+  public:
+    SD1ToneCoefficients();
+    
+    void set_tone(DataType alpha);
+    DataType get_tone() const;
+  };
+}
+
+#endif
