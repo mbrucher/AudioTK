9ebbcc75b0e76b4057402dfdef4f463eac9764ba
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-06-17 11:38:35 +0100

    Adding medium and high tests: the stack is definitely NOT behaving as I thought -> mistake somewhere.

--- tests/EQ/ToneStackFilter.cpp
+++ tests/EQ/ToneStackFilter.cpp
@@ -266,3 +266,507 @@ BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_low05_10k_test )
 
   checker.process(PROCESSSIZE);
 }
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_middle0_100_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(100);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_middle(0);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 1.27317612576));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_middle0_1k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(1000);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_middle(0);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0.528928508155));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_middle0_10k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(10000);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_middle(0);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0.778820213842));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_middle1_100_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(100);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_middle(1);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 1.27317612576));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_middle1_1k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(1000);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_middle(1);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0.528928508155));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_middle1_10k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(10000);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_middle(1);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0.778758646157));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_middle05_100_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(100);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_middle(.5);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 1.27317612576));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_middle05_1k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(1000);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_middle(.5);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0.528928508155));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_middle05_10k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(10000);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_middle(.5);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0.778820213842));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_high0_100_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(100);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_high(0);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 1.27317612576));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_high0_1k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(1000);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_high(0);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0.528928508155));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_high0_10k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(10000);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_high(0);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0.778820213842));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_high1_100_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(100);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_high(1);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 1.27317612576));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_high1_1k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(1000);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_high(1);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0.528928508155));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_high1_10k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(10000);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_high(1);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0.778758646157));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_high05_100_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(100);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_high(.5);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 1.27317612576));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_high05_1k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(1000);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_high(.5);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0.528928508155));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_ToneStackFilter_Bassman_high05_10k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(SAMPLINGRATE);
+  generator.set_amplitude(1);
+  generator.set_frequency(10000);
+
+  std::unique_ptr<ATK::ToneStackFilterCoefficients<double> > filter(ATK::ToneStackFilterCoefficients<double>::buildBassmanStack());
+  filter->set_input_sampling_rate(SAMPLINGRATE);
+  filter->set_output_sampling_rate(SAMPLINGRATE);
+  filter->set_high(.5);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(SAMPLINGRATE);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0.778820213842));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, filter.get(), 0);
+  filter->set_input_port(0, &generator, 0);
+
+  filter->process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
