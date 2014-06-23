872294195b1b964f18ebf2366a24575d2d2871da
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-06-23 11:52:42 +0100

    Starting the filter that will drive the VCA-like compressor

---
+++ ATK/Dynamic/PowerFilter.cpp
@@ -0,0 +1,55 @@
+/**
+ * \file PowerFilter.cpp
+ */
+
+#include "PowerFilter.h"
+
+#include <cmath>
+#include <cstdint>
+
+namespace ATK
+{
+  template<typename DataType_>
+  PowerFilter<DataType_>::PowerFilter(int nb_channels)
+  :Parent(nb_channels, nb_channels), memory_factor(0)
+  {
+    output_delay = 1;
+  }
+  
+  template<typename DataType_>
+  PowerFilter<DataType_>::~PowerFilter()
+  {
+    
+  }
+
+  template<typename DataType_>
+  void PowerFilter<DataType_>::set_memory(DataType_ memory_factor)
+  {
+    if(memory_factor < 0 || memory_factor >= 1)
+    {
+      throw std::range_error("Memory factor must be a positive value less than 1 (so that it doesn't diverge)");
+    }
+    this->memory_factor = memory_factor;
+  }
+  
+  template<typename DataType_>
+  DataType_ PowerFilter<DataType_>::get_memory() const
+  {
+    return memory_factor;
+  }
+  
+  template<typename DataType_>
+  void PowerFilter<DataType_>::process_impl(std::int64_t size)
+  {
+    for(int channel = 0; channel < nb_input_ports; ++channel)
+    {
+      for(std::int64_t i = 0; i < size; ++i)
+      {
+        outputs[channel][i] = converted_inputs[channel][i] * converted_inputs[channel][i] / 2 + memory_factor * outputs[channel][i-1];
+      }
+    }
+  }
+  
+  template class PowerFilter<float>;
+  template class PowerFilter<double>;
+}
---
+++ tests/Dynamic/main.cpp
@@ -0,0 +1,9 @@
+/**
+ * \ file main.pp
+ * Runner file for ATK Dynamic
+ */
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE ATKDynamic_test
+#include <boost/test/unit_test.hpp>
