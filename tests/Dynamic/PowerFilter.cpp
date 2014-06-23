5cc0212eddb8b11aeb469c512c9900ebaeb886c9
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-06-23 14:31:48 +0100

    Added test for different values.

--- tests/Dynamic/PowerFilter.cpp
+++ tests/Dynamic/PowerFilter.cpp
@@ -13,6 +13,18 @@
 
 #define PROCESSSIZE (1024*64)
 
+BOOST_AUTO_TEST_CASE( PowerFilter_RMS_throw_memory_0_test )
+{
+  ATK::PowerFilter<double> filter;
+  BOOST_CHECK_THROW(filter.set_memory(-1), std::out_of_range);
+}
+
+BOOST_AUTO_TEST_CASE( PowerFilter_RMS_throw_memory_1_test )
+{
+  ATK::PowerFilter<double> filter;
+  BOOST_CHECK_THROW(filter.set_memory(1), std::out_of_range);
+}
+
 BOOST_AUTO_TEST_CASE( PowerFilter_RMS_0_1k_test )
 {
   ATK::SinusGeneratorFilter<double> generator;
@@ -28,6 +40,7 @@ BOOST_AUTO_TEST_CASE( PowerFilter_RMS_0_1k_test )
   ATK::FFTCheckerFilter<double> checker;
   checker.set_input_sampling_rate(1024*64);
   std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(0, 0.7071067811865475244));
   frequency_checks.push_back(std::make_pair(100, 0));
   frequency_checks.push_back(std::make_pair(1000, 0));
   frequency_checks.push_back(std::make_pair(2000, 0.5)); // Power will multiple frequency by 2
@@ -40,22 +53,6 @@ BOOST_AUTO_TEST_CASE( PowerFilter_RMS_0_1k_test )
   filter.process(1024*64);
 
   checker.process(PROCESSSIZE);
-}
-
-BOOST_AUTO_TEST_CASE( PowerFilter_RMS_throw_memory_0_test )
-{
-  ATK::PowerFilter<double> filter;
-  filter.set_input_sampling_rate(1024*64);
-  filter.set_output_sampling_rate(1024*64);
-  BOOST_CHECK_THROW(filter.set_memory(-1), std::out_of_range);
-}
-
-BOOST_AUTO_TEST_CASE( PowerFilter_RMS_throw_memory_1_test )
-{
-  ATK::PowerFilter<double> filter;
-  filter.set_input_sampling_rate(1024*64);
-  filter.set_output_sampling_rate(1024*64);
-  BOOST_CHECK_THROW(filter.set_memory(1), std::out_of_range);
 }
 
 BOOST_AUTO_TEST_CASE( PowerFilter_RMS_05_1k_test )
@@ -73,10 +70,216 @@ BOOST_AUTO_TEST_CASE( PowerFilter_RMS_05_1k_test )
   ATK::FFTCheckerFilter<double> checker;
   checker.set_input_sampling_rate(1024*64);
   std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(0, 1));
   frequency_checks.push_back(std::make_pair(100, 0));
   frequency_checks.push_back(std::make_pair(1000, 0));
   frequency_checks.push_back(std::make_pair(2000, 0.6947103730615206));
   frequency_checks.push_back(std::make_pair(4000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+
+  filter.process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( PowerFilter_RMS_09_1k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(1000);
+
+  ATK::PowerFilter<double> filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_memory(0.9);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(0, 2.236067977499789697));
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(2000, 1.098070570944812));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+
+  filter.process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( PowerFilter_RMS_0_100_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(100);
+
+  ATK::PowerFilter<double> filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_memory(0);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(0, 0.7071067811865475244));
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(200, 0.5)); // Power will multiple frequency by 2
+  frequency_checks.push_back(std::make_pair(400, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+
+  filter.process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( PowerFilter_RMS_05_100_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(100);
+
+  ATK::PowerFilter<double> filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_memory(0.5);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(0, 1));
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(200, 0.7069768533999591));
+  frequency_checks.push_back(std::make_pair(400, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+
+  filter.process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( PowerFilter_RMS_09_100_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(100);
+
+  ATK::PowerFilter<double> filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_memory(0.9);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(0, 2.236067977499789697));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(200, 1.568323054019139));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+
+  filter.process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( PowerFilter_RMS_0_10k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(10000);
+
+  ATK::PowerFilter<double> filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_memory(0);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(0, 0.7071067811865475244));
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  frequency_checks.push_back(std::make_pair(20000, 0.5)); // Power will multiple frequency by 2
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+
+  filter.process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( PowerFilter_RMS_05_10k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(10000);
+
+  ATK::PowerFilter<double> filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_memory(0.5);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(0, 1));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  frequency_checks.push_back(std::make_pair(20000, 0.4452828409472382));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+
+  filter.process(1024*64);
+
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( PowerFilter_RMS_09_10k_test )
+{
+  ATK::SinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(10000);
+
+  ATK::PowerFilter<double> filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_memory(0.9);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(0, 2.236067977499789697));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  frequency_checks.push_back(std::make_pair(20000, 0.4008154691677741));
   checker.set_checks(frequency_checks);
 
   checker.set_input_port(0, &filter, 0);
