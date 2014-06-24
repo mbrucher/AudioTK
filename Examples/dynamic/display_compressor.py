1ab929555ca0fdc59c2b49ff6058000b87c8abe4
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-06-24 16:50:25 +0100

    Fix the gain compressor Python wrapper
    Started a compressor Python example

---
+++ Examples/dynamic/display_compressor.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+from ATK.Core import DoubleInPointerFilter, DoubleOutPointerFilter
+from ATK.Dynamic import DoubleApplyGainFilter, DoubleAttackReleaseFilter, DoubleGainCompressorFilter, DoublePowerFilter
+
+import matplotlib.pyplot as plt
+
+sample_rate = 96000
+
+
+def plot_me(signal, MySampleRate, NFFT = 8192, noverlap = 1024):
+  a = plt.subplot(2, 1, 1)
+  plt.title("Original signal")
+  plt.xlabel("s")
+  plt.ylabel("Hz")
+  plt.specgram(signal[0], NFFT = NFFT, Fs = MySampleRate, noverlap = noverlap )
+  a = plt.subplot(2, 1, 2)
+  plt.title("Processed signal")
+  plt.xlabel("s")
+  plt.ylabel("Hz")
+  plt.specgram(signal[1], NFFT = NFFT, Fs = MySampleRate, noverlap = noverlap )
+
+def filter(input):
+  import numpy as np
+  output = np.zeros(input.shape, dtype=np.float64)
+
+  infilter = DoubleInPointerFilter(input, False)
+  infilter.set_input_sampling_rate(sample_rate)
+
+  powerfilter = DoublePowerFilter(1)
+  powerfilter.set_input_sampling_rate(sample_rate)
+  powerfilter.set_input_port(0, infilter, 0)
+  powerfilter.set_memory(np.exp(-1/(sample_rate*50e-3)))
+
+  attackreleasefilter = DoubleAttackReleaseFilter(1)
+  attackreleasefilter.set_input_sampling_rate(sample_rate)
+  attackreleasefilter.set_input_port(0, powerfilter, 0)
+  attackreleasefilter.set_attack(np.exp(-1/(sample_rate*1e-3)))
+  attackreleasefilter.set_release(np.exp(-1/(sample_rate*100e-3)))
+
+  gainfilter = DoubleGainCompressorFilter(1)
+  gainfilter.set_input_sampling_rate(sample_rate)
+  gainfilter.set_input_port(0, attackreleasefilter, 0)
+  gainfilter.set_threshold(0.1)
+  gainfilter.set_slope(4)
+  gainfilter.set_softness(1)
+
+  outfilter = DoubleOutPointerFilter(output, False)
+  outfilter.set_input_sampling_rate(sample_rate)
+  outfilter.set_input_port(0, gainfilter, 0)
+  outfilter.process(input.shape[1])
+
+  return output
+
+if __name__ == "__main__":
+  import numpy as np
+  size = 100000
+
+  samples = 2000000
+  freq_max = 20000
+
+  t = np.arange(samples, dtype=np.float64).reshape(1, -1) / sample_rate
+  d = np.sin(np.pi * (sample_rate * freq_max / samples * (t + .1)) * t)
+
+  np.savetxt("input.txt", d)
+  out = filter(d)
+  plt.figure()
+  plt.title("Oversampling 32")
+  plot_me((d[0], out[0]), sample_rate)
+  plt.show()
--- Python/Dynamic/GainCompressorFilter.i
+++ Python/Dynamic/GainCompressorFilter.i
@@ -13,9 +13,12 @@ namespace ATK
     ~GainCompressorFilter();
 
     void set_threshold(DataType threshold);
+    void set_threshold_db(DataType threshold);
     DataType get_threshold() const;
     void set_slope(DataType slope);
     DataType get_slope() const;
+    void set_softness(DataType softness);
+    DataType get_softness() const;
   };
 }
 
