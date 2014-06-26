8ee13ffa2a10dce4a9b8f0e77aecfdd160bef86b
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-06-26 23:46:04 +0100

    Added a new example and enhanced display module search

--- Examples/delay/universaldelay.py
+++ Examples/delay/universaldelay.py
@@ -7,10 +7,6 @@ import matplotlib.pyplot as plt
 
 sample_rate = 96000
 
-import sys, os
-sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/..")
-from display.compare_spec import plot_me
-
 def filter(input, blend=0, feedback=0, feedforward=1):
   import numpy as np
   output = np.zeros(input.shape, dtype=np.float64)
@@ -35,17 +31,11 @@ def filter(input, blend=0, feedback=0, feedforward=1):
 
 if __name__ == "__main__":
   import numpy as np
-  samples = 2000000
-  freq_max = 20000
-
-  t = np.arange(samples, dtype=np.float64).reshape(1, -1) / sample_rate
-  d = np.sin(np.pi * (sample_rate * freq_max / samples * (t + .1)) * t)
+  size = 960000
+  
+  x = np.arange(size, dtype=np.float64).reshape(1, -1) / sample_rate
+  d = np.sin(x * 2 * np.pi * 1000)
 
   np.savetxt("input.txt", d)
   out = filter(d, feedforward=-1, blend=1, feedback=-.5)
   np.savetxt("output.txt", d)
-  plt.figure()
-  plot_me((d[0], out[0]), sample_rate)
-  plt.gcf().suptitle("Delay")
-  plt.legend()
-  plt.show()
--- Examples/dynamic/compressor.py
+++ Examples/dynamic/compressor.py
@@ -8,18 +8,6 @@ import matplotlib.pyplot as plt
 
 sample_rate = 96000
 
-def plot_me(signal, MySampleRate, NFFT = 8192, noverlap = 1024):
-  a = plt.subplot(2, 1, 1)
-  plt.title("Original signal")
-  plt.xlabel("s")
-  plt.ylabel("Hz")
-  plt.specgram(signal[0], NFFT = NFFT, Fs = MySampleRate, noverlap = noverlap )
-  a = plt.subplot(2, 1, 2)
-  plt.title("Processed signal")
-  plt.xlabel("s")
-  plt.ylabel("Hz")
-  plt.specgram(signal[1], NFFT = NFFT, Fs = MySampleRate, noverlap = noverlap )
-
 def filter(input):
   import numpy as np
   output = np.zeros(input.shape, dtype=np.float64)
