1bad78bb5f1d026e90bfc987c866db65215423ce
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-06-26 11:42:56 +0100

    Fixed delay line + tests

---
+++ tests/Delay/FixedDelayLineFilter.cpp
@@ -0,0 +1,54 @@
+/**
+ * \ file FixedDelayLineFilter.cpp
+ */
+
+#include <ATK/Delay/FixedDelayLineFilter.h>
+
+#include <ATK/Core/InPointerFilter.h>
+#include <ATK/Core/OutPointerFilter.h>
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_NO_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include <boost/math/constants/constants.hpp>
+
+#define PROCESSSIZE (1024*64)
+
+BOOST_AUTO_TEST_CASE( FixedDelayLineFilter_sinus_line100_delay50_test )
+{
+  boost::scoped_array<float> data(new float[PROCESSSIZE]);
+  for(std::int64_t i = 0; i < PROCESSSIZE; ++i)
+  {
+    data[i] = i / 48000;
+  }
+  
+  ATK::InPointerFilter<float> generator(data.get(), 1, PROCESSSIZE, false);
+  generator.set_output_sampling_rate(48000);
+
+  boost::scoped_array<float> outdata(new float[PROCESSSIZE]);
+
+  ATK::FixedDelayLineFilter<float> filter(100);
+  filter.set_input_sampling_rate(48000);
+  filter.set_input_port(0, &generator, 0);
+  filter.set_delay(50);
+
+  ATK::OutPointerFilter<float> output(outdata.get(), 1, PROCESSSIZE, false);
+  output.set_input_sampling_rate(48000);
+  output.set_input_port(0, &filter, 0);
+
+  output.process(1);
+  output.process(49);
+  output.process(51);
+  output.process(PROCESSSIZE - 1 - 49 -51);
+  
+  for(std::int64_t i = 0; i < 50; ++i)
+  {
+    BOOST_REQUIRE_EQUAL(0, outdata[i]);
+  }
+  
+  for(std::int64_t i = 50; i < PROCESSSIZE; ++i)
+  {
+    BOOST_REQUIRE_EQUAL(data[i - 50], outdata[i]);
+  }
+}
