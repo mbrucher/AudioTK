d5efa8e3a67ff6b389b83189fd4cc59352f588db
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-06-26 15:27:37 +0100

    Testing additional options in the delay line

--- tests/Delay/UniversalFixedDelayLineFilter.cpp
+++ tests/Delay/UniversalFixedDelayLineFilter.cpp
@@ -37,8 +37,8 @@ BOOST_AUTO_TEST_CASE( UniversalFixedDelayLineFilter_sinus_line100_delay50_test )
   output.set_input_sampling_rate(48000);
   output.set_input_port(0, &filter, 0);
 
-  output.process(1);
   output.process(49);
+  output.process(1);
   output.process(51);
   output.process(PROCESSSIZE - 1 - 49 -51);
   
@@ -50,5 +50,77 @@ BOOST_AUTO_TEST_CASE( UniversalFixedDelayLineFilter_sinus_line100_delay50_test )
   for(std::int64_t i = 50; i < PROCESSSIZE; ++i)
   {
     BOOST_REQUIRE_EQUAL(data[i - 50], outdata[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( UniversalFixedDelayLineFilter_sinus_line25_delay24_blend_1_feedforward_1_feedback_0_test )
+{
+  boost::scoped_array<float> data(new float[PROCESSSIZE]);
+  for(std::int64_t i = 0; i < PROCESSSIZE; ++i)
+  {
+    data[i] = std::sin(2 * boost::math::constants::pi<float>() * (i+1.)/48000 * 1000);
+  }
+
+  ATK::InPointerFilter<float> generator(data.get(), 1, PROCESSSIZE, false);
+  generator.set_output_sampling_rate(48000);
+
+  boost::scoped_array<float> outdata(new float[PROCESSSIZE]);
+
+  ATK::UniversalFixedDelayLineFilter<float> filter(25);
+  filter.set_input_sampling_rate(48000);
+  filter.set_input_port(0, &generator, 0);
+  filter.set_delay(24);
+  filter.set_blend(1);
+  filter.set_feedback(0);
+  filter.set_feedforward(1);
+
+  ATK::OutPointerFilter<float> output(outdata.get(), 1, PROCESSSIZE, false);
+  output.set_input_sampling_rate(48000);
+  output.set_input_port(0, &filter, 0);
+
+  output.process(49);
+  output.process(1);
+  output.process(51);
+  output.process(PROCESSSIZE - 1 - 49 -51);
+
+  for(std::int64_t i = 24; i < PROCESSSIZE; ++i)
+  {
+    BOOST_REQUIRE_SMALL(outdata[i], 0.0001f);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( UniversalFixedDelayLineFilter_sinus_line25_delay24_blend_0_feedforward_0_feedback_1_test )
+{
+  boost::scoped_array<float> data(new float[PROCESSSIZE]);
+  for(std::int64_t i = 0; i < PROCESSSIZE; ++i)
+  {
+    data[i] = std::sin(2 * boost::math::constants::pi<float>() * (i+1.)/48000 * 1000);
+  }
+
+  ATK::InPointerFilter<float> generator(data.get(), 1, PROCESSSIZE, false);
+  generator.set_output_sampling_rate(48000);
+
+  boost::scoped_array<float> outdata(new float[PROCESSSIZE]);
+
+  ATK::UniversalFixedDelayLineFilter<float> filter(25);
+  filter.set_input_sampling_rate(48000);
+  filter.set_input_port(0, &generator, 0);
+  filter.set_delay(24);
+  filter.set_blend(0);
+  filter.set_feedback(1);
+  filter.set_feedforward(0);
+
+  ATK::OutPointerFilter<float> output(outdata.get(), 1, PROCESSSIZE, false);
+  output.set_input_sampling_rate(48000);
+  output.set_input_port(0, &filter, 0);
+
+  output.process(49);
+  output.process(1);
+  output.process(51);
+  output.process(PROCESSSIZE - 1 - 49 -51);
+
+  for(std::int64_t i = 24; i < PROCESSSIZE; ++i)
+  {
+    BOOST_REQUIRE_SMALL(outdata[i], 0.0001f);
   }
 }
