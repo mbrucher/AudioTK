e06fdc9bdd5cd278624a1eb396ad777588f08f38
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-06-27 21:31:31 +0200

    Added a new sinus generator, meant for slow sinus and delay lines

---
+++ ATK/Tools/CachedSinusGeneratorFilter.cpp
@@ -0,0 +1,80 @@
+/**
+ * \file CachedSinusGeneratorFilter.cpp
+ */
+
+#include "CachedSinusGeneratorFilter.h"
+
+#include <cmath>
+#include <cstdint>
+
+#include <boost/math/constants/constants.hpp>
+
+namespace ATK
+{
+  template<typename DataType_>
+  CachedSinusGeneratorFilter<DataType_>::CachedSinusGeneratorFilter(int periods, int seconds)
+  :Parent(0, 1), indice(0), periods(periods), seconds(seconds), volume(1), offset(0)
+  {
+    
+  }
+  
+  template<typename DataType_>
+  CachedSinusGeneratorFilter<DataType_>::~CachedSinusGeneratorFilter()
+  {
+    
+  }
+  
+  template<typename DataType_>
+  void CachedSinusGeneratorFilter<DataType_>::set_volume(double volume)
+  {
+    this->volume = volume;
+  }
+  
+  template<typename DataType_>
+  double CachedSinusGeneratorFilter<DataType_>::get_volume() const
+  {
+    return volume;
+  }
+  
+  template<typename DataType_>
+  void CachedSinusGeneratorFilter<DataType_>::set_offset(double offset)
+  {
+    this->offset = offset;
+  }
+  
+  template<typename DataType_>
+  double CachedSinusGeneratorFilter<DataType_>::get_offset() const
+  {
+    return offset;
+  }
+
+  template<typename DataType_>
+  void CachedSinusGeneratorFilter<DataType_>::setup()
+  {
+    cache.resize(input_sampling_rate * seconds);
+    for(int i = 0; i < cache.size(); ++i)
+    {
+      cache[i] = std::sin(2 * boost::math::constants::pi<double>() * i * periods / seconds / input_sampling_rate);
+    }
+  }
+  
+  template<typename DataType_>
+  void CachedSinusGeneratorFilter<DataType_>::process_impl(std::int64_t size)
+  {
+    DataType* ATK_RESTRICT output = outputs[0];
+    for(std::int64_t i = 0; i < size; ++i)
+    {
+      output[i] = static_cast<DataType>(offset + volume * cache[indice++]);
+      if(indice >= cache.size())
+      {
+        indice = 0;
+      }
+    }
+  }
+  
+  template class CachedSinusGeneratorFilter<std::int16_t>;
+  template class CachedSinusGeneratorFilter<std::int32_t>;
+  template class CachedSinusGeneratorFilter<std::int64_t>;
+  template class CachedSinusGeneratorFilter<float>;
+  template class CachedSinusGeneratorFilter<double>;
+}
---
+++ ATK/Tools/CachedSinusGeneratorFilter.h
@@ -0,0 +1,55 @@
+/**
+ * \file CachedSinusGeneratorFilter.h
+ */
+
+#ifndef ATK_TOOLS_CACHEDSINUSGENERATORFILTER_H
+#define ATK_TOOLS_CACHEDSINUSGENERATORFILTER_H
+
+#include <vector>
+
+#include <ATK/Core/TypedBaseFilter.h>
+#include "config.h"
+
+namespace ATK
+{
+  /**
+   *
+   */
+  template<typename DataType_>
+  class ATK_TOOLS_EXPORT CachedSinusGeneratorFilter : public TypedBaseFilter<DataType_>
+  {
+  protected:
+    typedef TypedBaseFilter<DataType_> Parent;
+    using typename Parent::DataType;
+    using Parent::converted_inputs_size;
+    using Parent::outputs_size;
+    using Parent::converted_inputs;
+    using Parent::outputs;
+    using Parent::input_sampling_rate;
+    using Parent::output_sampling_rate;
+
+  public:
+    CachedSinusGeneratorFilter(int periods, int seconds = 1);
+    ~CachedSinusGeneratorFilter();
+
+    void set_volume(double volume);
+    double get_volume() const;
+
+    void set_offset(double offset);
+    double get_offset() const;
+
+  protected:
+    virtual void process_impl(std::int64_t size);
+    virtual void setup();
+    
+  private:
+    int indice;
+    int periods;
+    int seconds;
+    double volume;
+    double offset;
+    std::vector<DataType_> cache;
+  };
+}
+
+#endif
