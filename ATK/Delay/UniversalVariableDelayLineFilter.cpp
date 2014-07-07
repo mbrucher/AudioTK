cc21ff0b5b5caab56283310415f163b1ae4f7410
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-07-07 15:57:39 +0100

    Disabling a test, as testing this condition is quite complex.

--- ATK/Delay/UniversalVariableDelayLineFilter.cpp
+++ ATK/Delay/UniversalVariableDelayLineFilter.cpp
@@ -110,7 +110,7 @@ namespace ATK
       delay_line_ptr[i] = (processed_input_ptr[i + max_delay - integer_delay[i]] - last_delay) * (1 - fractional_delay[i]) + processed_input_ptr[i + max_delay - integer_delay[i] - 1];
       processed_input_ptr[max_delay + i] = input1[i] + feedback * processed_input_ptr[max_delay + i - central_delay];
       output[i] = blend * processed_input_ptr[max_delay + i] + feedforward * delay_line_ptr[i];
-      last_delay = delay_line_ptr[i];
+      last_delay = delay_line_ptr[i]; // the reason why the test is not that simple!
     }
   }
   
