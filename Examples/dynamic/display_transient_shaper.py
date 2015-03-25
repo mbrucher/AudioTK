0c5a1cd20348bbeee1b997749240fd8b4c278b6f
Matthieu Brucher <matthieu.brucher@gmail.com>
2015-03-25 11:59:05 +0000

    New trial, still not perfect, perhaps because of the data!

--- Examples/dynamic/display_transient_shaper.py
+++ Examples/dynamic/display_transient_shaper.py
@@ -11,7 +11,9 @@ from ATK.Tools import DoubleApplyGainFilter
 
 # get from http://www.telefunken-elektroakustik.com/download/brew/
 
-sampling_rate, data = wavfile.read("DRUM ROOM - U47  - BAE 1073.wav")
+filename = "DRUM ROOM - U47  - BAE 1073.wav"
+sampling_rate, data = wavfile.read(filename)
+processsize = len(data)
 
 data = data.reshape(-1, 1)
 infilter = Int32InPointerFilter(data, True)
@@ -25,14 +27,14 @@ powerfilter.set_memory(np.exp(-1/(sampling_rate*.1e-3)))
 attackreleasefilter = DoubleAttackReleaseFilter(1)
 attackreleasefilter.set_input_sampling_rate(sampling_rate)
 attackreleasefilter.set_input_port(0, powerfilter, 0)
-attackreleasefilter.set_attack(np.exp(-1/(sampling_rate*.1e-3)))
-attackreleasefilter.set_release(np.exp(-1/(sampling_rate*1e-3)))
+attackreleasefilter.set_attack(np.exp(-1/(sampling_rate*.01e-3)))
+attackreleasefilter.set_release(np.exp(-1/(sampling_rate*1.e-3)))
 
 gainfilter = DoubleGainCompressorFilter(1)
 gainfilter.set_input_sampling_rate(sampling_rate)
 gainfilter.set_input_port(0, attackreleasefilter, 0)
 gainfilter.set_threshold(.5)
-gainfilter.set_ratio(2)
+gainfilter.set_ratio(.5)
 gainfilter.set_softness(1)
 
 applygainfilter = DoubleApplyGainFilter(1)
@@ -40,22 +42,22 @@ applygainfilter.set_input_sampling_rate(sampling_rate)
 applygainfilter.set_input_port(0, gainfilter, 0)
 applygainfilter.set_input_port(1, infilter, 0)
 
-indata = np.zeros((len(data), 1), dtype=np.float32)
+indata = np.zeros((processsize, 1), dtype=np.float32)
 inputfilter = FloatOutPointerFilter(indata, True)
 inputfilter.set_input_sampling_rate(sampling_rate)
 inputfilter.set_input_port(0, infilter, 0)
 
-outdata = np.zeros((len(data), 1), dtype=np.float32)
+outdata = np.zeros((processsize, 1), dtype=np.float32)
 outfilter = FloatOutPointerFilter(outdata, True)
 outfilter.set_input_sampling_rate(sampling_rate)
 outfilter.set_input_port(0, applygainfilter, 0)
 
-outdata_power = np.zeros((len(data), 1), dtype=np.float32)
+outdata_power = np.zeros((processsize, 1), dtype=np.float32)
 outfilter_power = FloatOutPointerFilter(outdata_power, True)
 outfilter_power.set_input_sampling_rate(sampling_rate)
 outfilter_power.set_input_port(0, powerfilter, 0)
 
-outdata_gain = np.zeros((len(data), 1), dtype=np.float32)
+outdata_gain = np.zeros((processsize, 1), dtype=np.float32)
 outfilter_gain = FloatOutPointerFilter(outdata_gain, True)
 outfilter_gain.set_input_sampling_rate(sampling_rate)
 outfilter_gain.set_input_port(0, gainfilter, 0)
@@ -66,10 +68,10 @@ pipelineend.add_filter(inputfilter)
 pipelineend.add_filter(outfilter)
 pipelineend.add_filter(outfilter_power)
 pipelineend.add_filter(outfilter_gain)
-pipelineend.process(len(data))
+pipelineend.process(processsize)
 
-start = 9000
-stop = 10000
+start = 90000
+stop = 100000
 
 x = np.arange(stop, dtype=np.float32) / sampling_rate
 
