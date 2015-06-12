a300da891ae55d617841f34f71afbb941d6f3c54
Matthieu Brucher <matthieu.brucher@gmail.com>
2015-06-12 23:01:07 +0100

    Using a common base class for LUT computation, reuse main process function

---
+++ ATK/Dynamic/GainFilter.cpp
@@ -0,0 +1,105 @@
+/**
+ * \file GainFilter.cpp
+ */
+
+#include "GainFilter.h"
+
+#include <cmath>
+#include <cstdint>
+#include <stdexcept>
+
+namespace ATK
+{
+  template<typename DataType_>
+  GainFilter<DataType_>::GainFilter(int nb_channels, size_t LUTsize, size_t LUTprecision)
+  :Parent(nb_channels, nb_channels), threshold(1), ratio(1), softness(static_cast<DataType_>(.0001)), LUTsize(LUTsize), LUTprecision(LUTprecision)
+  {
+    gainLUT.reserve(LUTsize);
+  }
+  
+  template<typename DataType_>
+  GainFilter<DataType_>::~GainFilter()
+  {
+  }
+
+  template<typename DataType_>
+  void GainFilter<DataType_>::process_impl(std::int64_t size) const
+  {
+    assert(nb_input_ports == nb_output_ports);
+
+    for(int channel = 0; channel < nb_output_ports; ++channel)
+    {
+      const DataType* ATK_RESTRICT input = converted_inputs[channel];
+      DataType* ATK_RESTRICT output = outputs[channel];
+      for(std::int64_t i = 0; i < size; ++i)
+      {
+        DataType_ value = input[i] * threshold;
+        int step = static_cast<int>(value * LUTprecision);
+        if(step >= LUTsize)
+        {
+          step = static_cast<int>(LUTsize) - 1;
+        }
+        output[i] = gainLUT[step];
+      }
+    }
+  }
+  
+  template<typename DataType_>
+  void GainFilter<DataType_>::set_threshold(DataType_ threshold)
+  {
+    if(threshold <= 0)
+    {
+      throw std::out_of_range("Threshold factor must be strictly positive value");
+    }
+    this->threshold = 1 / threshold;
+  }
+
+  template<typename DataType_>
+  void GainFilter<DataType_>::set_threshold_db(DataType_ threshold_db)
+  {
+    this->threshold = static_cast<DataType_>(std::pow(10, - threshold_db / 10));
+  }
+
+  template<typename DataType_>
+  DataType_ GainFilter<DataType_>::get_threshold() const
+  {
+    return 1 / threshold;
+  }
+
+  template<typename DataType_>
+  void GainFilter<DataType_>::set_ratio(DataType_ ratio)
+  {
+    if(ratio <= 0)
+    {
+      throw std::out_of_range("Ratio factor must be higher than 0");
+    }
+    this->ratio = ratio;
+    recomputeLUT();
+  }
+
+  template<typename DataType_>
+  DataType_ GainFilter<DataType_>::get_ratio() const
+  {
+    return ratio;
+  }
+
+  template<typename DataType_>
+  void GainFilter<DataType_>::set_softness(DataType_ softness)
+  {
+    if(softness <= 0)
+    {
+      throw std::out_of_range("Softness factor must be strictly positive value");
+    }
+    this->softness = softness;
+    recomputeLUT();
+  }
+
+  template<typename DataType_>
+  DataType_ GainFilter<DataType_>::get_softness() const
+  {
+    return softness;
+  }
+
+  template class GainFilter<float>;
+  template class GainFilter<double>;
+}
---
+++ ATK/Dynamic/GainFilter.h
@@ -0,0 +1,61 @@
+/**
+ * \file GainFilter.h
+ */
+
+#ifndef ATK_DYNAMIC_GAINFILTER_H
+#define ATK_DYNAMIC_GAINFILTER_H
+
+#include <vector>
+
+#include <ATK/Core/TypedBaseFilter.h>
+#include "config.h"
+
+namespace ATK
+{
+  /**
+   * Gain generic filter. Based on a LUT table, compute the gain.
+   */
+  template<typename DataType_>
+  class ATK_DYNAMIC_EXPORT GainFilter : public TypedBaseFilter<DataType_>
+  {
+  protected:
+    typedef TypedBaseFilter<DataType_> Parent;
+    using typename Parent::DataType;
+    using Parent::converted_inputs_size;
+    using Parent::outputs_size;
+    using Parent::converted_inputs;
+    using Parent::outputs;
+    using Parent::nb_input_ports;
+    using Parent::nb_output_ports;
+
+  public:
+    GainFilter(int nb_channels = 1, size_t LUTsize = 128*1024, size_t LUTprecision = 64);
+    ~GainFilter();
+    
+    void set_threshold(DataType_ threshold);
+    void set_threshold_db(DataType_ threshold_db);
+    DataType_ get_threshold() const;
+    /**
+     * 1:ratio is defined as the reduction factor. 1:1 is a reduction of 0, 1:2 is a reduction by a factor of 2...
+     */
+    void set_ratio(DataType_ ratio);
+    DataType_ get_ratio() const;
+    void set_softness(DataType_ softness);
+    DataType_ get_softness() const;
+
+  protected:
+    virtual void process_impl(std::int64_t size) const override final;
+
+    virtual void recomputeLUT() = 0;
+    
+    DataType_ threshold;
+    DataType_ ratio;
+    DataType_ softness;
+    size_t LUTsize;
+    size_t LUTprecision;
+
+    std::vector<DataType_> gainLUT;
+  };
+}
+
+#endif
