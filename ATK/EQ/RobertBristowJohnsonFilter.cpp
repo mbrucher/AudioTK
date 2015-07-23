fe141cca6a7572bf72c133aebd1bc2e7a187182e
Matthieu Brucher <matthieu.brucher@gmail.com>
2015-07-23 17:11:15 +0100

    All pass filter

--- ATK/EQ/RobertBristowJohnsonFilter.cpp
+++ ATK/EQ/RobertBristowJohnsonFilter.cpp
@@ -164,15 +164,56 @@ namespace ATK
   {
     return Q;
   }
+
+  template<typename DataType>
+  RobertBristowJohnsonAllPassCoefficients<DataType>::RobertBristowJohnsonAllPassCoefficients(int nb_channels)
+    :Parent(nb_channels), Q(1)
+  {
+  }
+
+  template <typename DataType>
+  void RobertBristowJohnsonAllPassCoefficients<DataType>::setup()
+  {
+    Parent::setup();
+
+    DataType w0 = 2 * boost::math::constants::pi<DataType>() * cut_frequency / input_sampling_rate;
+    DataType cosw0 = std::cos(w0);
+    DataType alpha = std::sin(w0) / (2 * Q);
+
+    coefficients_in[2] = (1 - alpha) / (1 + alpha);
+    coefficients_in[1] = -2 * cosw0 / (1 + alpha);
+    coefficients_in[0] = 1;
+    coefficients_out[1] = 2 * cosw0 / (1 + alpha);
+    coefficients_out[0] = (alpha - 1) / (1 + alpha);
+  }
+
+  template <typename DataType_>
+  void RobertBristowJohnsonAllPassCoefficients<DataType_>::set_Q(DataType_ Q)
+  {
+    if (Q <= 0)
+    {
+      throw std::out_of_range("Q must be positive");
+    }
+    this->Q = Q;
+    setup();
+  }
+
+  template <typename DataType_>
+  DataType_ RobertBristowJohnsonAllPassCoefficients<DataType_>::get_Q() const
+  {
+    return Q;
+  }
+
   template class RobertBristowJohnsonLowPassCoefficients<float>;
   template class RobertBristowJohnsonLowPassCoefficients<double>;
   template class RobertBristowJohnsonHighPassCoefficients<float>;
   template class RobertBristowJohnsonHighPassCoefficients<double>;
-  
   template class RobertBristowJohnsonBandPassCoefficients<float>;
   template class RobertBristowJohnsonBandPassCoefficients<double>;
   template class RobertBristowJohnsonBandPass2Coefficients<float>;
   template class RobertBristowJohnsonBandPass2Coefficients<double>;
+  template class RobertBristowJohnsonAllPassCoefficients<float>;
+  template class RobertBristowJohnsonAllPassCoefficients<double>;
   template class IIRFilter<RobertBristowJohnsonLowPassCoefficients<float> >;
   template class IIRFilter<RobertBristowJohnsonLowPassCoefficients<double> >;
   template class IIRFilter<RobertBristowJohnsonHighPassCoefficients<float> >;
@@ -181,4 +222,6 @@ namespace ATK
   template class IIRFilter<RobertBristowJohnsonBandPassCoefficients<double> >;
   template class IIRFilter<RobertBristowJohnsonBandPass2Coefficients<float> >;
   template class IIRFilter<RobertBristowJohnsonBandPass2Coefficients<double> >;
+  template class IIRFilter<RobertBristowJohnsonAllPassCoefficients<float> >;
+  template class IIRFilter<RobertBristowJohnsonAllPassCoefficients<double> >;
 }
