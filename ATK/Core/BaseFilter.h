2d348292e01adba58b13b0ae628e70edd2ed9e08
Matthieu Brucher <matthieu.brucher@gmail.com>
2015-10-05 11:36:14 +0100

    Added thread pool mechanism

--- ATK/Core/BaseFilter.h
+++ ATK/Core/BaseFilter.h
@@ -16,6 +16,10 @@
 #include <boost/timer/timer.hpp>
 #endif
 
+#if ATK_USE_THREADPOOL == 1
+#include <tbb/queuing_mutex.h>
+#endif
+
 namespace ATK
 {
   class BaseFilter
@@ -37,6 +41,10 @@ namespace ATK
     /// Starts processing after calling reset
     ATK_CORE_EXPORT void process(int64_t size);
     
+#if ATK_USE_THREADPOOL == 1
+    ATK_CORE_EXPORT void process_parallel(int64_t size);
+#endif
+
     ATK_CORE_EXPORT void set_input_sampling_rate(int rate);
     ATK_CORE_EXPORT int get_input_sampling_rate() const;
     ATK_CORE_EXPORT void set_output_sampling_rate(int rate);
@@ -55,12 +63,16 @@ namespace ATK
     virtual int get_type() const = 0;
     /// Starts processing without calling reset
     void process_conditionnally(int64_t size);
+#if ATK_USE_THREADPOOL == 1
+    void process_conditionnally_parallel(int64_t size);
+#endif
     /// Resets the internal state of the filter (mandatory before processing a new clip in a DAW for instance)
     ATK_CORE_EXPORT virtual void full_setup();
   
   protected:
     /// The actual filter processing part
     virtual void process_impl(int64_t size) const = 0;
+
     /// Prepares the filter by retrieving the inputs arrays
     virtual void prepare_process(int64_t size) = 0;
     /// Prepares the filter by resizing the outputs arrays
@@ -87,7 +99,9 @@ namespace ATK
     boost::timer::nanosecond_type output_conversion_time;
     boost::timer::nanosecond_type process_time;
 #endif
-
+#if ATK_USE_THREADPOOL == 1
+    tbb::queuing_mutex mutex;
+#endif
   };
 }
 
