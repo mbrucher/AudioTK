cd026cf43d64e30e309b5ea9dcc3e0cdf846187e
Matthieu Brucher <matthieu.brucher@gmail.com>
2016-03-05 21:27:27 +0000

    Implement the all pass filter

--- ATK/Reverberation/AllPassFilter.h
+++ ATK/Reverberation/AllPassFilter.h
@@ -12,8 +12,6 @@
 
 namespace ATK
 {
-  template<typename DataType>
-  class UFDLF_Impl;
   /**
    * Gain "compressor". Has twice as many inputs channels as it has output channels
    * Even channels are signal, odd channels are gains, results is the product of both
@@ -27,6 +25,7 @@ namespace ATK
     using Parent::converted_inputs_size;
     using Parent::outputs_size;
     using Parent::converted_inputs;
+    using Parent::input_delay;
     using Parent::outputs;
     using Parent::nb_input_ports;
     using Parent::nb_output_ports;
@@ -39,25 +38,13 @@ namespace ATK
     void set_delay(int64_t delay);
     int64_t get_delay() const;
 
-    void set_blend(DataType_ blend);
-    DataType_ get_blend() const;
-
     void set_feedback(DataType_ feedback);
     DataType_ get_feedback() const;
-
-    void set_feedforward(DataType_ feedforward);
-    DataType_ get_feedforward() const;
-
-    virtual void full_setup() override final;
   protected:
     virtual void process_impl(int64_t size) const override final;
 
-    // internal state
-    std::unique_ptr<UFDLF_Impl<DataType_> > impl;
     int64_t delay;
-    DataType_ blend;
     DataType_ feedback;
-    DataType_ feedforward;
   };
 }
 
