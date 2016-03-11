e68771c9c380d2f21e6a8df02cc6b94c6f15a422
Matthieu Brucher <matthieu.brucher@gmail.com>
2016-03-11 11:40:39 +0000

    Doxygen comments for Utility Perhaps @param should not be phrases?

--- ATK/Utility/FFT.h
+++ ATK/Utility/FFT.h
@@ -21,23 +21,55 @@
 
 namespace ATK
 {
+  /// An FFT class
   template<class DataType_>
   class ATK_UTILITY_EXPORT FFT
   {
   public:
+    /// Builds the wrapper class
     FFT();
+    /// Destructor
     ~FFT();
     
+    /*!
+     * @brief Sets a new size for the FFT and creates an associated plan for it
+     * @ size is the new size of the plan
+     */
     void set_size(int64_t size);
+    /*!
+     * @brief Processes a FFT from a real input array and stores it internally
+     * @param input is the real input array of data to process
+     * @param input_size is the size of the input array
+     */
     void process(const DataType_* input, int64_t input_size) const;
+    /*!
+    * @brief Processes a FFT from a real input to a complex output
+    * @param input is the real input array of data to process
+    * @param output is the complex output array
+    * @param input_size is the size of both input and output arrays
+    */
     void process_forward(const DataType_* input, std::complex<DataType_>* output, int64_t input_size) const;
+    /*!
+    * @brief Processes an inverse FFT from a complex input to a real output
+    * @param input is the complex input array of data to process
+    * @param output is the real output array
+    * @param input_size is the size of both input and output arrays
+    */
     void process_backward(const std::complex<DataType_>* input, DataType_* output, int64_t input_size) const;
+    /*!
+    * @brief Computes the amplitude of the resulting spectrum
+    * @param amp is the output angle container
+    */
     void get_amp(std::vector<DataType_>& amp) const;
+    /*!
+    * @brief Computes the angle of the resulting spectrum
+    * @param angle is the output angle container
+    */
     void get_angle(std::vector<DataType_>& angle) const;
 
   protected:
-	int64_t size;
-	int log2n;
+	  int64_t size;
+	  int log2n;
 
 #if ATK_USE_FFTW == 1
     fftw_plan fft_plan;
--- ATK/Utility/exp.h
+++ ATK/Utility/exp.h
@@ -12,17 +12,30 @@
 
 namespace ATK
 {
+  /// A natural exponential approximation class
+  /**
+   * A natural exponential approximation class, using a LUT to approximate positive exponent values, and the inverse for negative ones
+   */
   template<class DataType_>
   class ATK_UTILITY_EXPORT Exp
   {
   public:
+    /*!
+     * @brief Constructor, with the number of steps and scale
+     * @param max is the maximum value that the instance will try to approximate
+     * @steps is the number of steps and the size of the LUT table
+     */
     Exp(DataType_ max, int steps);
 
+    /// Returns the approximation of the natural exponential of the input value
     DataType_ operator()(DataType_ value) const;
 
   protected:
+    /// The internal LUT table
     std::vector<DataType_> LUT;
+    /// Coefficient to unscale the input value from the LUT table
     DataType_ scale;
+    /// Inverse coefficient to scale the input value to the LUT table
     DataType_ invscale;
   };
 }
