5098a324c3b789301ed02e252ad45e6dd26268c4
Matthieu Brucher <matthieu.brucher@gmail.com>
2016-03-12 23:51:54 +0000

    Documentation for reverberation

--- ATK/Reverberation/AllPassReverbFilter.h
+++ ATK/Reverberation/AllPassReverbFilter.h
@@ -12,13 +12,12 @@
 
 namespace ATK
 {
-  /**
-   * Simple all pass reverb filter
-   */
+  /// Simple all pass reverb filter
   template<typename DataType_>
   class ATK_REVERBERATION_EXPORT AllPassReverbFilter : public TypedBaseFilter<DataType_>
   {
   protected:
+    /// Simplify parent calls
     typedef TypedBaseFilter<DataType_> Parent;
     using typename Parent::DataType;
     using Parent::converted_inputs;
@@ -27,17 +26,27 @@ namespace ATK
     using Parent::output_delay;
 
   public:
+    /*!
+    * @brief construct the filter with a maximum delay line size
+    * @param max-delay is the maximum delay allowed
+    */
     AllPassReverbFilter(int max_delay);
+    /// Destructor
     ~AllPassReverbFilter();
 
+    /// Changes the delay used for the filter
     void set_delay(int64_t delay);
+    /// Returns the elay used for the system
     int64_t get_delay() const;
 
+    /// Sets feedback amount (between -1 and 1)
     void set_feedback(DataType_ feedback);
+    /// Gets feedback amount
     DataType_ get_feedback() const;
   protected:
     virtual void process_impl(int64_t size) const override final;
 
+  private:
     int64_t delay;
     DataType_ feedback;
   };
--- ATK/Reverberation/LowPassReverbFilter.h
+++ ATK/Reverberation/LowPassReverbFilter.h
@@ -12,14 +12,12 @@
 
 namespace ATK
 {
-  /**
-   * Gain "compressor". Has twice as many inputs channels as it has output channels
-   * Even channels are signal, odd channels are gains, results is the product of both
-   */
+  /// Simple low pass reverb filter
   template<typename DataType_>
   class ATK_REVERBERATION_EXPORT LowPassReverbFilter : public TypedBaseFilter<DataType_>
   {
   protected:
+    /// Simplify parent calls
     typedef TypedBaseFilter<DataType_> Parent;
     using typename Parent::DataType;
     using Parent::converted_inputs_size;
@@ -32,19 +30,30 @@ namespace ATK
     using Parent::output_delay;
 
   public:
+    /*!
+    * @brief construct the filter with a maximum delay line size
+    * @param max-delay is the maximum delay allowed
+    */
     LowPassReverbFilter(int max_delay);
+    /// Destructor
     ~LowPassReverbFilter();
 
+    /// Changes the delay used for the filter
     void set_delay(int64_t delay);
+    /// Returns the elay used for the system
     int64_t get_delay() const;
 
+    /// Sets feedback amount (sum of cutoff and feedback must be between 0 and 1)
     void set_feedback(DataType_ feedback);
+    /// Gets feedback amount
     DataType_ get_feedback() const;
+    /// Sets cutoff amount (sum of cutoff and feedback must be between 0 and 1)
     void set_cutoff(DataType_ cutoff);
+    /// Gets cutoff amount
     DataType_ get_cutoff() const;
   protected:
     virtual void process_impl(int64_t size) const override final;
-
+  private:
     int64_t delay;
     DataType_ feedback;
     DataType_ cutoff;
