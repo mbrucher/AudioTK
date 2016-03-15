fa9041049c5c323ad4da4e453a6bae04f8e459fd
Matthieu Brucher <matthieu.brucher@gmail.com>
2016-03-15 16:43:41 +0000

    Adding doc for libsndfile filters

--- ATK/IO/libsndfile/OutSndFileFilter.h
+++ ATK/IO/libsndfile/OutSndFileFilter.h
@@ -15,10 +15,12 @@ class SndfileHandle;
 
 namespace ATK
 {
+  /// A sink filter for sound files supported by lbsnd
   template<typename DataType_>
   class OutSndFileFilter: public TypedBaseFilter<DataType_>
   {
   public:
+    /// Simplify parent calls
     typedef TypedBaseFilter<DataType_> Parent;
     using typename Parent::DataType;
     using Parent::converted_inputs_size;
@@ -34,7 +36,13 @@ namespace ATK
     void setup() override final;
     
   public:
+    /*!
+     * @brief Constructor
+     * @param filename is the name of the output file
+     * @param ports is the number of output channels
+     */
     OutSndFileFilter(const std::string& filename, int ports);
+    /// Destructor
     ~OutSndFileFilter();
   };
 }
