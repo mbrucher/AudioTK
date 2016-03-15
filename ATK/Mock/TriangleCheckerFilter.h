57cbc301d3346129a1b5ee55c15404293e13b434
Matthieu Brucher <matthieu.brucher@gmail.com>
2016-03-15 16:48:34 +0000

    Mock documentation update

--- ATK/Mock/TriangleCheckerFilter.h
+++ ATK/Mock/TriangleCheckerFilter.h
@@ -11,25 +11,32 @@
 
 namespace ATK
 {
+  ///  Filter that checks that the input is a synced triangle signal
   template<class DataType_>
   class ATK_MOCK_EXPORT TriangleCheckerFilter : public TypedBaseFilter<DataType_>
   {
   public:
+    /// Simplify parent calls
     typedef TypedBaseFilter<DataType_> Parent;
     using typename Parent::DataType;
     using Parent::converted_inputs_size;
     using Parent::converted_inputs;
     using Parent::input_sampling_rate;
 
+    /// Constructor
     TriangleCheckerFilter();
+    /// Destructor
     virtual ~TriangleCheckerFilter();
     
-    void set_amplitude(DataType amplitude);
+    /// Sets the amplitude of the triangle
+    void set_amplitude(DataType_ amplitude);
+    /// Sets the frequency of the triangle
     void set_frequency(int frequency);
 
   protected:
     virtual void process_impl(int64_t size) const override final;
     
+  private:
     mutable double state;
     mutable bool ascending;
     DataType amplitude;
