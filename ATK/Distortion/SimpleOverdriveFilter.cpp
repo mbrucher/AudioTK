7419be75791e41c5a847280cf8a77ab587e131be
Matthieu Brucher <matthieu.brucher@gmail.com>
2016-03-21 17:42:20 +0100

    Added Forward and Backward Euler for testing purposes

--- ATK/Distortion/SimpleOverdriveFilter.cpp
+++ ATK/Distortion/SimpleOverdriveFilter.cpp
@@ -79,7 +79,6 @@ namespace ATK
     }
   };
   
-  
   template <typename DataType>
   SimpleOverdriveFilter<DataType>::SimpleOverdriveFilter()
   :TypedBaseFilter<DataType>(1, 1)
@@ -104,17 +103,160 @@ namespace ATK
   {
     assert(nb_input_ports == nb_output_ports);
 
-    for(int channel = 0; channel < nb_input_ports; ++channel)
+    const DataType* ATK_RESTRICT input = converted_inputs[0];
+    DataType* ATK_RESTRICT output = outputs[0];
+    for(int64_t i = 0; i < size; ++i)
     {
-      const DataType* ATK_RESTRICT input = converted_inputs[channel];
-      DataType* ATK_RESTRICT output = outputs[channel];
-      for(int64_t i = 0; i < size; ++i)
-      {
-        output[i] = optimizer->optimize(input[i]);
-      }
+      output[i] = optimizer->optimize(input[i]);
     }
   }
 
   template class SimpleOverdriveFilter<float>;
   template class SimpleOverdriveFilter<double>;
+
+  template<typename DataType_>
+  class BackwardSimpleOverdriveFilter<DataType_>::SimpleOverdriveFunction
+  {
+  public:
+    typedef DataType_ DataType;
+  protected:
+    DataType A;
+    DataType B;
+    DataType is;
+    DataType vt;
+    
+    DataType oldy0;
+    DataType oldexpy0;
+    DataType oldinvexpy0;
+    DataType oldy1;
+    DataType oldexpy1;
+    DataType oldinvexpy1;
+  public:
+    SimpleOverdriveFunction(DataType dt, DataType R, DataType C, DataType is, DataType vt)
+    :is(is), vt(vt)
+    {
+      A = dt / C + R;
+      B = - R;
+      
+      oldy0 = oldy1 = 0;
+      oldexpy0 = oldinvexpy0 = oldexpy1 = oldinvexpy1 = 1;
+    }
+    
+    std::pair<DataType, DataType> operator()(DataType x0, DataType x1, DataType y0, DataType y1)
+    {
+      DataType expdiode_y1_p = std::exp(y1 / vt);
+      DataType expdiode_y1_m = 1 / expdiode_y1_p;
+      DataType expdiode_y0_p;
+      DataType expdiode_y0_m;
+      
+      if(y0 == oldy0)
+      {
+        expdiode_y0_p = oldexpy0;
+        expdiode_y0_m = oldinvexpy0;
+      }
+      else if(y0 == oldy1)
+      {
+        expdiode_y0_p = oldexpy1;
+        expdiode_y0_m = oldinvexpy1;
+      }
+      else
+      {
+        expdiode_y0_p = std::exp(y0 / vt);
+        expdiode_y0_m = 1 / expdiode_y0_p;
+      }
+      
+      oldy0 = y0;
+      oldexpy0 = expdiode_y0_p;
+      oldinvexpy0 = expdiode_y0_m;
+      
+      oldy1 = y1;
+      oldexpy1 = expdiode_y1_p;
+      oldinvexpy1 = expdiode_y1_m;
+      
+      std::pair<DataType, DataType> diode = std::make_pair(is * (expdiode_y1_p - expdiode_y1_m), is * (expdiode_y1_p + expdiode_y1_m) / vt);
+      return std::make_pair(A * diode.first + (y1 + (x0 - x1 + B * is * (expdiode_y0_p - expdiode_y0_m) - y0)), A * diode.second + 1);
+    }
+    
+    DataType estimate(DataType x0, DataType x1, DataType y0)
+    {
+      return y0;
+    }
+  };
+
+  template <typename DataType>
+  BackwardSimpleOverdriveFilter<DataType>::BackwardSimpleOverdriveFilter()
+  :TypedBaseFilter<DataType>(1, 1)
+  {
+  }
+  
+  template <typename DataType>
+  BackwardSimpleOverdriveFilter<DataType>::~BackwardSimpleOverdriveFilter()
+  {
+  }
+  
+  template <typename DataType>
+  void BackwardSimpleOverdriveFilter<DataType>::setup()
+  {
+    Parent::setup();
+    optimizer.reset(new ScalarNewtonRaphson<SimpleOverdriveFunction>(SimpleOverdriveFunction(static_cast<DataType>(1. / input_sampling_rate),
+                                                                                             10000, static_cast<DataType>(22e-9), static_cast<DataType>(1e-12), static_cast<DataType>(26e-3))));
+  }
+  
+  template <typename DataType>
+  void BackwardSimpleOverdriveFilter<DataType>::process_impl(int64_t size) const
+  {
+    assert(nb_input_ports == nb_output_ports);
+    
+    const DataType* ATK_RESTRICT input = converted_inputs[0];
+    DataType* ATK_RESTRICT output = outputs[0];
+    for(int64_t i = 0; i < size; ++i)
+    {
+      output[i] = optimizer->optimize(input[i]);
+    }
+  }
+  
+  template class BackwardSimpleOverdriveFilter<float>;
+  template class BackwardSimpleOverdriveFilter<double>;
+  
+  template <typename DataType>
+  ForwardSimpleOverdriveFilter<DataType>::ForwardSimpleOverdriveFilter()
+  :TypedBaseFilter<DataType>(1, 1)
+  {
+    input_delay = 1;
+    output_delay = 1;
+  }
+  
+  template <typename DataType>
+  ForwardSimpleOverdriveFilter<DataType>::~ForwardSimpleOverdriveFilter()
+  {
+  }
+  
+  template <typename DataType>
+  void ForwardSimpleOverdriveFilter<DataType>::setup()
+  {
+    Parent::setup();
+  }
+  
+  template <typename DataType>
+  void ForwardSimpleOverdriveFilter<DataType>::process_impl(int64_t size) const
+  {
+    assert(nb_input_ports == nb_output_ports);
+    
+    DataType dt = static_cast<DataType>(1. / input_sampling_rate);
+    DataType R = 10000;
+    DataType C = static_cast<DataType>(22e-9);
+    DataType is = static_cast<DataType>(1e-12);
+    DataType vt = static_cast<DataType>(26e-3);
+    
+    const DataType* ATK_RESTRICT input = converted_inputs[0];
+    DataType* ATK_RESTRICT output = outputs[0];
+    for(int64_t i = 0; i < size; ++i)
+    {
+      output[i] = output[i-1] + (input[i] - input[i-1] - (4 * dt * is * C * sinh(output[i-1] / vt))) / (1 + 2 * is * R * vt * cosh(output[i-1] / vt));
+    }
+  }
+  
+  template class ForwardSimpleOverdriveFilter<float>;
+  template class ForwardSimpleOverdriveFilter<double>;
+
 }
--- ATK/Distortion/SimpleOverdriveFilter.h
+++ ATK/Distortion/SimpleOverdriveFilter.h
@@ -32,9 +32,6 @@ namespace ATK
     using Parent::outputs;
     using Parent::input_sampling_rate;
     using Parent::output_sampling_rate;
-    
-    using Parent::nb_input_ports;
-    using Parent::nb_output_ports;
   public:
     /*!
     * @brief Constructor
@@ -49,6 +46,68 @@ namespace ATK
     
   private:
     std::unique_ptr<ScalarNewtonRaphson<SimpleOverdriveFunction> > optimizer;
+  };
+
+  /// Diode clipper filter, based on a backward Euler rule
+  template<typename DataType_>
+  class ATK_DISTORTION_EXPORT BackwardSimpleOverdriveFilter: public TypedBaseFilter<DataType_>
+  {
+    class SimpleOverdriveFunction;
+  public:
+    /// Simplify parent calls
+    typedef TypedBaseFilter<DataType_> Parent;
+    using typename Parent::DataType;
+    using Parent::converted_inputs_size;
+    using Parent::outputs_size;
+    using Parent::converted_inputs;
+    using Parent::outputs;
+    using Parent::input_sampling_rate;
+    using Parent::output_sampling_rate;
+  public:
+    /*!
+     * @brief Constructor
+     */
+    BackwardSimpleOverdriveFilter();
+    /// Destructor
+    ~BackwardSimpleOverdriveFilter();
+    
+  protected:
+    void setup() override final;
+    void process_impl(int64_t size) const override final;
+    
+  private:
+    std::unique_ptr<ScalarNewtonRaphson<SimpleOverdriveFunction> > optimizer;
+  };
+
+  /// Diode clipper filter, based on a forward Euler rule
+  template<typename DataType_>
+  class ATK_DISTORTION_EXPORT ForwardSimpleOverdriveFilter: public TypedBaseFilter<DataType_>
+  {
+    class SimpleOverdriveFunction;
+  public:
+    /// Simplify parent calls
+    typedef TypedBaseFilter<DataType_> Parent;
+    using typename Parent::DataType;
+    using Parent::converted_inputs_size;
+    using Parent::outputs_size;
+    using Parent::converted_inputs;
+    using Parent::outputs;
+    using Parent::input_sampling_rate;
+    using Parent::output_sampling_rate;
+    
+    using Parent::input_delay;
+    using Parent::output_delay;
+  public:
+    /*!
+     * @brief Constructor
+     */
+    ForwardSimpleOverdriveFilter();
+    /// Destructor
+    ~ForwardSimpleOverdriveFilter();
+    
+  protected:
+    void setup() override final;
+    void process_impl(int64_t size) const override final;
   };
 }
 
