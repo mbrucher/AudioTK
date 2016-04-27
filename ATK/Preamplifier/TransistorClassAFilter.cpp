3537bd0e5696e533848ecb80c50fce95376d32df
Matthieu Brucher <matthieu.brucher@gmail.com>
2016-04-27 18:43:13 +0200

    Starting implementation of the emitter stabilisation equation

--- ATK/Preamplifier/TransistorClassAFilter.cpp
+++ ATK/Preamplifier/TransistorClassAFilter.cpp
@@ -16,10 +16,12 @@ namespace ATK
     const DataType_ dt;
     
     const DataType_ Rp;
-    const DataType_ Rg;
+    const DataType_ Rg1;
+    const DataType_ Rg2;
     const DataType_ Ro;
     const DataType_ Rk;
     const DataType_ VBias;
+    const DataType_ Cg;
     const DataType_ Co;
     const DataType_ Ck;
     
@@ -64,7 +66,7 @@ namespace ATK
     typedef Eigen::Matrix<DataType, 4, 4> Matrix;
     
     TransistorClassAFunction(DataType dt)
-    :dt(dt), Rp(100e3), Rg(220e3), Ro(22e3), Rk(2.7e3), VBias(12), Co(20e-9), Ck(10e-6), Is(1e-12), Vt(26e-3), Br(1), Bf(100)
+    :dt(dt), Rp(1.5e3), Rg1(10e3), Rg2(2.2e3), Ro(22e3), Rk(470), VBias(12), Cg(1e-9), Co(100e-9), Ck(10e-3), Is(1e-12), Vt(26e-3), Br(1), Bf(100)
     {
     }
 
@@ -95,24 +97,25 @@ namespace ATK
 
       auto f1_old = - output[0][i-1] / (Rk * Ck) + (Ib_old + Ic_old) / Ck;
       auto f2_old = - (output[1][i-1] + output[2][i-1]) / (Ro * Co);
+      auto f4_old = 1/Cg * (output[3][i-1] * (1/Rg1 + 1/Rg2) - VBias/Rg1 + Ib_old);
 
       auto f1 = - y1(0) / (Rk * Ck) + (Ib + Ic) / Ck;
       auto f2 = - (y1(1) + y1(2)) / (Ro * Co);
+      auto f4 = 1/Cg * (y1(3) * (1/Rg1 + 1/Rg2) - VBias/Rg1 + Ib);
 
       auto g1 = y1(2) + Rp * (Ic + (y1(1) + y1(2)) / Ro) - VBias;
-      auto g2 = y1(3) - input[0][i] + Rg * Ib;
       
       Vector F(Vector::Zero());
       F << (dt / 2 * (f1 + f1_old) + output[0][i-1] - y1(0)),
            (dt / 2 * (f2 + f2_old) + output[1][i-1] - y1(1)),
            (g1),
-           (g2);
+           (dt / 2 * (f4 + f4_old) + output[3][i-1] - y1(3) + input[0][i] - input[0][i-1]);
 
       Matrix M(Matrix::Zero());
       M << (-1 -dt/2/(Rk * Ck))+ dt/2*((Ib_Vbe + Ic_Vbe + Ib_Vce + Ic_Vce)/ Ck), 0, -dt/2*(Ib_Vce + Ic_Vce) / Ck, -dt/2*(Ib_Vbe + Ic_Vbe) / Ck,
             0, -1 -dt/2*(Ro * Co), -dt/2*(Ro * Co), 0,
             Rp * (Ic_Vbe + Ic_Vce), Rp / Ro, 1 + Rp / Ro - Rp * Ic_Vce, - Rp * Ic_Vbe,
-            Rg * (Ib_Vbe + Ib_Vce), 0, -Rg * Ib_Vce, -Rg * Ib_Vbe + 1;
+            dt/2 * (Ib_Vce + Ib_Vbe)/Cg, 0, -dt/2 * Ib_Vce/Cg, -1 + dt/2 * (1/Cg * ((1/Rg1 + 1/Rg2) - Ib_Vbe));
       
       return std::make_pair(F, M);
     }
