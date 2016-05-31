3f868be2232cf31d093785966e636e0f61514ba4
Matthieu Brucher <matthieu.brucher@gmail.com>
2016-05-31 22:01:45 +0100

    Rename

--- ATK/EQ/ToneStackFilter.h
+++ ATK/EQ/ToneStackFilter.h
@@ -9,12 +9,12 @@
 
 namespace ATK
 {
-  template<typename DataType_>
+  template<typename Coeffs>
   class IIRFilter;
 
   /// Emulation of a classic tone stack section of an amp
   template<typename DataType_>
-  class ToneStackFilterCoefficients: public TypedBaseFilter<DataType_>
+  class ToneStackCoefficients: public TypedBaseFilter<DataType_>
   {
   public:
     /// Simplify parent calls
@@ -41,10 +41,10 @@ namespace ATK
      * @brief Constructor
      * @param nb_channels is the number of input and output channels
      */
-    ToneStackFilterCoefficients(int nb_channels = 1);
+    ToneStackCoefficients(int nb_channels = 1);
   public:
     /// Move constructor
-    ToneStackFilterCoefficients(ToneStackFilterCoefficients&& other);
+    ToneStackCoefficients(ToneStackCoefficients&& other);
 
     /// Changes the low section parameter of the stack
     void set_low(DataType_ alpha);
@@ -60,9 +60,9 @@ namespace ATK
     DataType_ get_high() const;
     
     /// Builds a Bassman stack equivalent filter (bass, Fender)
-    static IIRFilter<ToneStackFilterCoefficients<DataType_> >&& buildBassmanStack();
+    static IIRFilter<ToneStackCoefficients<DataType_> > buildBassmanStack();
     /// Builds a JCM800 stack equivalent filter (guitar, Marshall)
-    static IIRFilter<ToneStackFilterCoefficients<DataType_> >&& buildJCM800Stack();
+    static IIRFilter<ToneStackCoefficients<DataType_> > buildJCM800Stack();
 
   protected:
   /// Sets the specific coefficients for a given stack
