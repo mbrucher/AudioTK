49e1abb2aad1b175f4e4ae21fbc5b5cc4cfee134
Matthieu Brucher <matthieu.brucher@gmail.com>
2016-10-27 16:30:05 +0100

    Cleaning up

--- profiling/EQ/main.cpp
+++ profiling/EQ/main.cpp
@@ -3,7 +3,6 @@
 #include <ATK/EQ/BesselFilter.h>
 #include <ATK/EQ/IIRFilter.h>
 
-#include <ATK/Mock/FFTCheckerFilter.h>
 #include <ATK/Mock/SimpleSinusGeneratorFilter.h>
 
 int main(int argc, char** argv)
@@ -19,19 +18,10 @@ int main(int argc, char** argv)
   filter.set_cut_frequency(100);
   filter.set_order(3);
 
-  ATK::FFTCheckerFilter<double> checker;
-  checker.set_input_sampling_rate(1024 * 64);
-  std::vector<std::pair<int, double> > frequency_checks;
-  frequency_checks.push_back(std::make_pair(100, 0));
-  frequency_checks.push_back(std::make_pair(1000, 0.03150874916131525));
-  frequency_checks.push_back(std::make_pair(10000, 0));
-  checker.set_checks(frequency_checks);
-
-  checker.set_input_port(0, &filter, 0);
   filter.set_input_port(0, &generator, 0);
 
   for(size_t i = 0; i < 1024*1024; ++i)
-    checker.process(1024);
+    filter.process(1024);
 
   return EXIT_SUCCESS;
 }
