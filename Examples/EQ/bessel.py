a2a035a7e5af47c3c2b704653b8b925485e325a7
Matthieu Brucher <matthieu.brucher@gmail.com>
2016-11-24 15:08:03 +0000

    Python 3

--- Examples/EQ/bessel.py
+++ Examples/EQ/bessel.py
@@ -14,12 +14,12 @@ def filter_low(input):
   lowpassfilter.set_cut_frequency(1000)
   lowpassfilter.set_order(5)
   lowpassfilter.set_input_port(0, infilter, 0)
-  print lowpassfilter.get_coefficients_in()
-  print lowpassfilter.get_coefficients_out()
+  print(lowpassfilter.get_coefficients_in())
+  print(lowpassfilter.get_coefficients_out())
   outfilter = DoubleOutPointerFilter(output, False)
   outfilter.set_input_sampling_rate(48000)
   outfilter.set_input_port(0, lowpassfilter, 0)
-  outfilter.process(input.shape[1])
+  #outfilter.process(input.shape[1])
   return output
 
 def filter_high(input):
@@ -33,12 +33,12 @@ def filter_high(input):
   highpassfilter.set_cut_frequency(1000)
   highpassfilter.set_order(5)
   highpassfilter.set_input_port(0, infilter, 0)
-  print highpassfilter.get_coefficients_in()
-  print highpassfilter.get_coefficients_out()
+  print(highpassfilter.get_coefficients_in())
+  print(highpassfilter.get_coefficients_out())
   outfilter = DoubleOutPointerFilter(output, False)
   outfilter.set_input_sampling_rate(48000)
   outfilter.set_input_port(0, highpassfilter, 0)
-  outfilter.process(input.shape[1])
+  #outfilter.process(input.shape[1])
   return output
 
 def filter_band(input):
@@ -52,12 +52,12 @@ def filter_band(input):
   bandpassfilter.set_cut_frequencies(200, 1000)
   bandpassfilter.set_order(5)
   bandpassfilter.set_input_port(0, infilter, 0)
-  print bandpassfilter.get_coefficients_in()
-  print bandpassfilter.get_coefficients_out()
+  print(bandpassfilter.get_coefficients_in())
+  print(bandpassfilter.get_coefficients_out())
   outfilter = DoubleOutPointerFilter(output, False)
   outfilter.set_input_sampling_rate(48000)
   outfilter.set_input_port(0, bandpassfilter, 0)
-  outfilter.process(input.shape[1])
+  #outfilter.process(input.shape[1])
   return output
 
 def filter_bandstop(input):
@@ -71,24 +71,24 @@ def filter_bandstop(input):
   bandstopfilter.set_cut_frequencies(200, 1000)
   bandstopfilter.set_order(5)
   bandstopfilter.set_input_port(0, infilter, 0)
-  print bandstopfilter.get_coefficients_in()
-  print bandstopfilter.get_coefficients_out()
+  print(bandstopfilter.get_coefficients_in())
+  print(bandstopfilter.get_coefficients_out())
   outfilter = DoubleOutPointerFilter(output, False)
   outfilter.set_input_sampling_rate(48000)
   outfilter.set_input_port(0, bandstopfilter, 0)
-  outfilter.process(input.shape[1])
+  #outfilter.process(input.shape[1])
   return output
 
 if __name__ == "__main__":
   import numpy as np
   d = np.arange(1000000).reshape(1, -1)
   d = np.sin(d * 2 * np.pi * 1000 / 48000)
-  print d[0,0:100]
+  print(d[0,0:100])
   out = filter_low(d)
-  print out[0,0:100]
+  print(out[0,0:100])
   out = filter_high(d)
-  print out[0,0:100]
+  print(out[0,0:100])
   out = filter_band(d)
-  print out[0,0:100]
+  print(out[0,0:100])
   out = filter_bandstop(d)
-  print out[0,0:100]
+  print(out[0,0:100])
