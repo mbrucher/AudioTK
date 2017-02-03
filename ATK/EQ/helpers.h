cd802c6dd9ee5959db61e7fe2123a8bfd0edad90
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-02-03 14:25:20 +0000

    There is an issue in the polynomial in some way?

--- ATK/EQ/helpers.h
+++ ATK/EQ/helpers.h
@@ -165,7 +165,6 @@ namespace
     auto p_size = p.size();
     std::cout << "zpk2ba" << std::endl;
     std::cout << z_size << std::endl;
-    std::cout << p_size << std::endl;
     b = boost::math::tools::polynomial<DataType>(k);
     
     for(int i = 0; i < z_size; ++i)
@@ -184,6 +183,7 @@ namespace
       }
     }
     
+    std::cout << p_size << std::endl;
     a = boost::math::tools::polynomial<DataType>(1);
     for(size_t i = 0; i < p_size; ++i)
     {
