776fb95eb06e5d6fe517765aab535b97200d0c7a
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-02-03 11:32:32 +0000

    OK, the error happens during init, probably a bad vector call. Let's see in more details.

--- tests/EQ/BesselFilter.cpp
+++ tests/EQ/BesselFilter.cpp
@@ -21,13 +21,17 @@ BOOST_AUTO_TEST_CASE( IIRFilter_BesselLowPassCoefficients_1k_test )
   generator.set_output_sampling_rate(1024*64);
   generator.set_amplitude(1);
   generator.set_frequency(1000);
-  std::cout << "OK" << std::endl;
+
   ATK::IIRFilter<ATK::BesselLowPassCoefficients<double> > filter;
-  filter.set_input_sampling_rate(1024*64);
-  filter.set_output_sampling_rate(1024*64);
-  filter.set_cut_frequency(100);
-  filter.set_order(3);
   std::cout << "OK" << std::endl;
+  filter.set_input_sampling_rate(1024*64);
+  std::cout << "OK" << std::endl;
+  filter.set_output_sampling_rate(1024*64);
+  std::cout << "OK" << std::endl;
+  filter.set_cut_frequency(100);
+  std::cout << "OK" << std::endl;
+  filter.set_order(3);
+  
   ATK::FFTCheckerFilter<double> checker;
   checker.set_input_sampling_rate(1024*64);
   std::vector<std::pair<int, double> > frequency_checks;
@@ -35,14 +39,13 @@ BOOST_AUTO_TEST_CASE( IIRFilter_BesselLowPassCoefficients_1k_test )
   frequency_checks.push_back(std::make_pair(1000, 0.03150874916131525));
   frequency_checks.push_back(std::make_pair(10000, 0));
   checker.set_checks(frequency_checks);
-  std::cout << "OK" << std::endl;
+  
   checker.set_input_port(0, &filter, 0);
   filter.set_input_port(0, &generator, 0);
-  std::cout << "OK" << std::endl;
+  
   filter.process(1024*64);
-  std::cout << "OK" << std::endl;
+  
   checker.process(PROCESSSIZE);
-  std::cout << "OK" << std::endl;
 }
 
 BOOST_AUTO_TEST_CASE( IIRFilter_BesselLowPassCoefficients_100_test )
