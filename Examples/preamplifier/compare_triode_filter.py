960e427a487ffc73b1c76c588c24ebdf55a2e3a4
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-03-05 12:29:21 +0000

    Additional python scripts to see the differences between triode models

---
+++ Examples/preamplifier/compare_triode_filter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from enhanced_tube_filter import filter_4 as filter_4_ek
+from leach_tube_filter import filter_4 as filter_4_l
+from tube_filter import filter_4 as filter_4_k
+
+
+if __name__ == "__main__":
+  import numpy as np
+  size = 1200
+  
+  x = np.arange(size).reshape(1, -1) / 48000.
+  d = np.sin(x * 2 * np.pi * 200) * 10
+  d.tofile("input.dat")
+
+  import matplotlib.pyplot as plt
+  plt.plot(x[0], d[0], label="input")
+
+  out = filter_4_ek(d)
+  out.tofile("output4_ek.dat")
+  plt.plot(x[0], out[0], label="Enhanced Koren")
+  out = filter_4_k(d)
+  out.tofile("output4_k.dat")
+  plt.plot(x[0], out[0], label="Koren")
+  out = filter_4_l(d)
+  out.tofile("output4_l.dat")
+  plt.plot(x[0], out[0], label="Leach")
+
+  plt.legend()
+  plt.show()
+
