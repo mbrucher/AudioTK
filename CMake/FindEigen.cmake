83be3f670e6789e575c3cc562a8db148b50ab0b0
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-03-20 10:15:47 +0000

    This is incredible. I must have made a stupid mistake somewhere??

--- CMake/FindEigen.cmake
+++ CMake/FindEigen.cmake
@@ -94,8 +94,11 @@ LIST(APPEND EIGEN_CHECK_PATH_SUFFIXES
   /
   eigen3 # Default root directory for Eigen.
   Eigen/include/eigen3 ) # Windows (for C:/Program Files prefix).
-MESSAGE(status ${EIGEN_ROOT})
-MESSAGE(status ${EIGEN_CANDIDATES})
+
+file(GLOB EIGEN_CANDIDATES2 ${EIGEN_ROOT}/*/Eigen/Core)
+MESSAGE(status ${EIGEN_CANDIDATES2})
+file(GLOB EIGEN_CANDIDATES2 ${EIGEN_ROOT}/*/*/Eigen/Core)
+MESSAGE(status ${EIGEN_CANDIDATES2})
 # Search supplied hint directories first if supplied.
 FIND_PATH(EIGEN_INCLUDE_DIR
   NAMES Eigen/Core
