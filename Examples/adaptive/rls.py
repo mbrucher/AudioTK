2d45fa26cabf992cf6c9ab2a108e3a21c8cfc3c1
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-03-23 14:08:28 +0000

    Show that not learning makes the filter stable

--- Examples/adaptive/rls.py
+++ Examples/adaptive/rls.py
@@ -17,15 +17,15 @@ def filter(input):
   outfilter = DoubleOutPointerFilter(output, False)
   outfilter.set_input_sampling_rate(48000)
   outfilter.set_input_port(0, rls, 0)
-  outfilter.process(input.shape[1]//2)
+  outfilter.process(1000)
   rls.set_learning(False)
-  outfilter.process(input.shape[1]//2)
+  outfilter.process(input.shape[1] - 1000)
 
   return output
 
 if __name__ == "__main__":
   import numpy as np
-  size = 1200
+  size = 5000
   
   x = np.arange(size).reshape(1, -1) / 48000.
   d = np.sin(x * 2 * np.pi * 100)
