6cf8301fdd4c50519020f318365236e611436bfb
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-03-24 10:15:30 +0000

    New test

--- tests/Adaptive/RLSFilter.cpp
+++ tests/Adaptive/RLSFilter.cpp
@@ -20,7 +20,19 @@
 
 #define PROCESSSIZE (1024*64)
 
-BOOST_AUTO_TEST_CASE( RLSFilter_memory_negative_test )
+BOOST_AUTO_TEST_CASE(RLSFilter_size_negative_test)
+{
+  ATK::RLSFilter<float> filter(100);
+  BOOST_CHECK_THROW(filter.set_size(0), std::out_of_range);
+}
+
+BOOST_AUTO_TEST_CASE(RLSFilter_size_set_test)
+{
+  ATK::RLSFilter<float> filter(100);
+  filter.set_size(10);
+}
+
+BOOST_AUTO_TEST_CASE(RLSFilter_memory_negative_test)
 {
   ATK::RLSFilter<float> filter(100);
   BOOST_CHECK_THROW(filter.set_memory(0), std::out_of_range);
@@ -127,6 +139,47 @@ BOOST_AUTO_TEST_CASE( RLSFilter_learning_test )
   output.process(PROCESSSIZE);
   
   for(std::int64_t i = 100; i < PROCESSSIZE; ++i) // let the RLS filter start learning first
+  {
+    BOOST_REQUIRE_SMALL(std::abs(outdata[i]), 0.1f);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(RLSFilter_learning_training_test)
+{
+  ATK::SimpleSinusGeneratorFilter<float> generator;
+  generator.set_output_sampling_rate(48000);
+  generator.set_amplitude(1);
+  generator.set_frequency(1000);
+
+  ATK::SimpleSinusGeneratorFilter<float> reference;
+  reference.set_output_sampling_rate(48000);
+  reference.set_amplitude(-1);
+  reference.set_frequency(1000);
+
+  ATK::RLSFilter<float> filter(10);
+  filter.set_input_sampling_rate(48000);
+  filter.set_output_sampling_rate(48000);
+  filter.set_memory(0.9999);
+  filter.set_learning(true);
+
+  ATK::SumFilter<float> sum;
+  sum.set_input_sampling_rate(48000);
+  sum.set_output_sampling_rate(48000);
+
+  std::array<float, PROCESSSIZE> outdata;
+  ATK::OutPointerFilter<float> output(outdata.data(), 1, PROCESSSIZE, false);
+  output.set_input_sampling_rate(48000);
+
+  filter.set_input_port(0, &generator, 0);
+  sum.set_input_port(0, &reference, 0);
+  sum.set_input_port(1, &filter, 0);
+  output.set_input_port(0, &sum, 0);
+
+  output.process(1000);
+  filter.set_learning(false);
+  output.process(PROCESSSIZE - 1000);
+
+  for (std::int64_t i = 100; i < PROCESSSIZE; ++i) // let the RLS filter start learning first
   {
     BOOST_REQUIRE_SMALL(std::abs(outdata[i]), 0.1f);
   }
