c3319ae75e798ba73dc8c5c1e77d41469e5bdcf2
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-04-09 20:46:09 +0100

    Adding a new test to track the refactor

--- tests/Delay/FixedDelayLineFilter.cpp
+++ tests/Delay/FixedDelayLineFilter.cpp
@@ -7,6 +7,11 @@
 #include <ATK/Core/InPointerFilter.h>
 #include <ATK/Core/OutPointerFilter.h>
 
+#include <ATK/Mock/SimpleSinusGeneratorFilter.h>
+#include <ATK/Mock/TriangleCheckerFilter.h>
+
+#include <ATK/Tools/SumFilter.h>
+
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_NO_MAIN
 #include <boost/test/unit_test.hpp>
@@ -62,5 +67,33 @@ BOOST_AUTO_TEST_CASE( FixedDelayLineFilter_sinus_line100_delay50_test )
   for(ptrdiff_t i = 50; i < PROCESSSIZE; ++i)
   {
     BOOST_REQUIRE_EQUAL(data[i - 50], outdata[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( FixedDelayLineFilter_sinus_line100_delay50_progressive_test )
+{
+  ATK::SimpleSinusGeneratorFilter<float> generator;
+  generator.set_output_sampling_rate(48000);
+  generator.set_frequency(480);
+  
+  ATK::FixedDelayLineFilter<float> filter(100);
+  filter.set_input_sampling_rate(48000);
+  filter.set_input_port(0, &generator, 0);
+  filter.set_delay(50);
+  filter.process(50);
+  
+  ATK::SumFilter<float> sum;
+  sum.set_input_sampling_rate(48000);
+  sum.set_input_port(0, &filter, 0);
+  sum.set_input_port(1, &generator, 0);
+  
+  ATK::TriangleCheckerFilter<float> output;
+  output.set_input_sampling_rate(48000);
+  output.set_input_port(0, &sum, 0);
+  output.set_amplitude(0);
+  
+  for(std::size_t i = 50; i < PROCESSSIZE; ++i)
+  {
+    output.process(i);
   }
 }
