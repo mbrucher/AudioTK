ecbb000d67f93a568cd21b199ca6c8aa968214d6
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-04-22 14:14:44 +0100

    Add a comment message as the gain factor is not correct. I should perhaps put it inside the transition matrix, as I'm not using simplified matrix multiply

--- ATK/Delay/HadamardMixture.h
+++ ATK/Delay/HadamardMixture.h
@@ -23,7 +23,7 @@ namespace ATK
     static const int nb_channels = 1 << order;
     typedef DataType_ DataType;
     /// Gain factor to take into account in the feedback loop due to the instability of the mixture
-    static constexpr double gain_factor = 0.5;
+    static constexpr double gain_factor = 0.5; // should be std::pow(2, -order / 2.);, but pow is not constexpr :/
 
     class MixtureImpl;
 
