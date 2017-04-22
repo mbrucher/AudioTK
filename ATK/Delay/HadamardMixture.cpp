0619bf2db0314bd61774aee2e28ec283ad11fde6
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-04-22 12:41:05 +0100

    Automated Hadamard build, get sizes 2, 4 and 8 by default (no Python wrappers for 2 and 8)

--- ATK/Delay/HadamardMixture.cpp
+++ ATK/Delay/HadamardMixture.cpp
@@ -11,6 +11,7 @@
 #include <cstring>
 #include <complex>
 #include <stdexcept>
+#include <type_traits>
 
 #include <ATK/Core/TypeTraits.h>
 
@@ -35,24 +36,50 @@ namespace ATK
       return transition * x;
     }
 
-    protected:
-      const Matrix transition;
-
-      Matrix create() const
-      {
-        Matrix transition;
-        transition << 1, 1, 1, 1,
-          1, -1, 1, -1,
-          -1, -1, 1, 1,
-          -1, 1, 1, -1;
-        return transition;
-      }
+  protected:
+    const Matrix transition;
+    
+    static Matrix create()
+    {
+      return recursive_create<order>();
+    }
+    
+    template<unsigned int recursive_order>
+    static typename std::enable_if<recursive_order == 0, Eigen::Matrix<DataType_, 1, 1>>::type recursive_create()
+    {
+      return Eigen::Matrix<DataType_, 1, 1>::Constant(1);
+    }
+    
+    template<unsigned int recursive_order>
+    static typename std::enable_if<recursive_order != 0, Eigen::Matrix<DataType_, 1<<recursive_order, 1<<recursive_order>>::type recursive_create()
+    {
+      constexpr auto big_size = 1 << recursive_order;
+      constexpr auto small_size = 1 << (recursive_order - 1);
+      Eigen::Matrix<DataType_, big_size, big_size> cur_transition;
+      
+      auto M_1 = recursive_create<recursive_order - 1>();
+      cur_transition.block(0, 0, small_size, small_size) = M_1;
+      cur_transition.block(0, small_size, small_size, small_size) = M_1;
+      cur_transition.block(small_size, 0, small_size, small_size) = -M_1;
+      cur_transition.block(small_size, small_size, small_size, small_size) = M_1;
+      return cur_transition;
+    }
   };
+
+  template class FeedbackDelayNetworkFilter<HadamardMixture<float, 1>>;
+  template class FeedbackDelayNetworkFilter<HadamardMixture<double, 1>>;
+  template class FeedbackDelayNetworkFilter<HadamardMixture<std::complex<float>, 1>>;
+  template class FeedbackDelayNetworkFilter<HadamardMixture<std::complex<double>, 1>>;
 
   template class FeedbackDelayNetworkFilter<HadamardMixture<float, 2>>;
   template class FeedbackDelayNetworkFilter<HadamardMixture<double, 2>>;
   template class FeedbackDelayNetworkFilter<HadamardMixture<std::complex<float>, 2>>;
   template class FeedbackDelayNetworkFilter<HadamardMixture<std::complex<double>, 2>>;
+
+  template class FeedbackDelayNetworkFilter<HadamardMixture<float, 3>>;
+  template class FeedbackDelayNetworkFilter<HadamardMixture<double, 3>>;
+  template class FeedbackDelayNetworkFilter<HadamardMixture<std::complex<float>, 3>>;
+  template class FeedbackDelayNetworkFilter<HadamardMixture<std::complex<double>, 3>>;
 }
 
 #endif
