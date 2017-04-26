8e74577479b526191a09c7273baff08f7edd1e35
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-04-26 09:26:16 +0100

    Some small API changes

--- ATK/Tools/BufferFilter.cpp
+++ ATK/Tools/BufferFilter.cpp
@@ -31,7 +31,7 @@ namespace ATK
       DataType* ATK_RESTRICT output = outputs[channel];
       for(std::size_t i = 0; i < size; ++i)
       {
-        *(output++) = *(input++);
+        output[i] = input[i];
       }
     }
   }
