9b703e0a9f06bf65d7b16744eeec89e3b9837582
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-05-12 20:27:43 +0100

    Adding preamp null tests

---
+++ tests/Python/Preamplifier/PyATKPreamplifier_FollowerTransistorClassA_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from nose.tools import raises
+from nose.plugins.attrib import attr
+
+sampling_rate = 48000 * 4
+
+@attr('Eigen')
+def DoubleFollowerTransistorClassAFilter_zero_test():
+  import numpy as np
+  from ATK.Core import DoubleInPointerFilter, DoubleOutPointerFilter
+  from ATK.Preamplifier import DoubleFollowerTransistorClassAFilter
+
+  from numpy.testing import assert_almost_equal
+  
+  input = np.zeros(1000, dtype=np.float64).reshape(1, -1)
+  output = np.zeros(1000, dtype=np.float64).reshape(1, -1)
+
+  inputfilter = DoubleInPointerFilter(input, False)
+  triodefilter = DoubleFollowerTransistorClassAFilter.build_standard_filter()
+  outputfilter = DoubleOutPointerFilter(output, False)
+
+  inputfilter.set_output_sampling_rate(sampling_rate)
+  triodefilter.set_input_sampling_rate(sampling_rate)
+  outputfilter.set_input_sampling_rate(sampling_rate)
+
+  triodefilter.set_input_port(0, inputfilter, 0)
+  outputfilter.set_input_port(0, triodefilter, 0)
+
+  outputfilter.process(1000)
+
+  assert_almost_equal(0, output[0])
+
---
+++ tests/Python/Preamplifier/PyATKPreamplifier_Triode2_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from nose.tools import raises
+from nose.plugins.attrib import attr
+
+sampling_rate = 48000 * 4
+
+@attr('Eigen')
+def DoubleKorenTriode2Filter_zero_test():
+  import numpy as np
+  from ATK.Core import DoubleInPointerFilter, DoubleOutPointerFilter
+  from ATK.Preamplifier import DoubleKorenTriode2Filter
+
+  from numpy.testing import assert_almost_equal
+  
+  input = np.zeros(1000, dtype=np.float64).reshape(1, -1)
+  output = np.zeros(1000, dtype=np.float64).reshape(1, -1)
+
+  inputfilter = DoubleInPointerFilter(input, False)
+  triodefilter = DoubleKorenTriode2Filter.build_standard_filter()
+  outputfilter = DoubleOutPointerFilter(output, False)
+
+  inputfilter.set_output_sampling_rate(sampling_rate)
+  triodefilter.set_input_sampling_rate(sampling_rate)
+  outputfilter.set_input_sampling_rate(sampling_rate)
+
+  triodefilter.set_input_port(0, inputfilter, 0)
+  outputfilter.set_input_port(0, triodefilter, 0)
+
+  outputfilter.process(1000)
+
+  assert_almost_equal(0, output[0])
