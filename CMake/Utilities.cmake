e8c46b1d3b01a7bc68826695b41f4837d754f778
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-09-09 21:04:28 +0100

    Fix EQ build and move Python to automated build

--- CMake/Utilities.cmake
+++ CMake/Utilities.cmake
@@ -6,7 +6,9 @@ function(ATK_add_library PREFIX)
 
 SOURCE_GROUP_BY_FOLDER(${PREFIX})
 
-include_directories(${PROJECT_SOURCE_DIR})
+add_definitions(${${PREFIX}_DEFINITIONS})
+
+include_directories(${PROJECT_SOURCE_DIR} ${${PREFIX}_INCLUDE})
 
 if(ENABLE_STATIC_LIBRARIES)
   add_library(${PREFIX}_static
@@ -31,7 +33,7 @@ if(ENABLE_SHARED_LIBRARIES)
 
   add_library(${${PREFIX}_NAME}
     SHARED
-    ${ATK_UTILITY_SRC} ${ATK_UTILITY_HEADERS} ${NATVIS_FILE}
+    ${${PREFIX}_SRC} ${${PREFIX}_HEADERS} ${NATVIS_FILE}
   )
 
   set_target_properties (${${PREFIX}_NAME} PROPERTIES
@@ -54,5 +56,58 @@ INSTALL(FILES ${${PREFIX}_HEADERS}
   DESTINATION ${CMAKE_INSTALL_PREFIX}/include/ATK/${${PREFIX}_FOLDER}
 )
 
+endfunction()
+
+function(ATK_add_executable PREFIX)
+
+SOURCE_GROUP_BY_FOLDER(${PREFIX})
+
+add_definitions(${${PREFIX}_DEFINITIONS})
+
+include_directories(${PROJECT_SOURCE_DIR} ${${PREFIX}_INCLUDE})
+
+add_executable(${${PREFIX}_NAME}
+  ${${PREFIX}_SRC} ${${PREFIX}_HEADERS} ${NATVIS_FILE}
+)
+
+set_target_properties (${${PREFIX}_NAME} PROPERTIES
+  FOLDER C++
+)
+
+target_link_libraries(${${PREFIX}_NAME} ${${PREFIX}_LIBRARIES})
+
+endfunction()
+
+function(ATK_add_test PREFIX)
+
+ATK_add_executable(${PREFIX})
+add_test(${${PREFIX}_TESTNAME} ${${PREFIX}_NAME} --log_level=message)
+
+set_target_properties (${${PREFIX}_NAME} PROPERTIES
+  FOLDER Tests
+)
+
+endfunction()
+
+
+function(ATK_add_python_module PREFIX)
+
+SOURCE_GROUP_BY_FOLDER(${PREFIX})
+
+include_directories(${PROJECT_SOURCE_DIR} ${NUMPY_INCLUDE_DIRS} ${${PREFIX}_INCLUDE})
+
+pybind11_add_module(${${PREFIX}_NAME} MODULE ${${PREFIX}_SRC} ${${PREFIX}_HEADERS} ${NATVIS_FILE})
+target_link_libraries(${${PREFIX}_NAME} PRIVATE ${${PREFIX}_LIBRARIES})
+
+INSTALL(FILES __init__.py
+  DESTINATION ${PYTHON_INSTALL_FOLDER}/site-packages/ATK/${${PREFIX}_FOLDER}
+)
+INSTALL(TARGETS ${${PREFIX}_NAME}
+  DESTINATION ${PYTHON_INSTALL_FOLDER}/site-packages/ATK/${${PREFIX}_FOLDER}
+)
+
+set_target_properties (${${PREFIX}_NAME} PROPERTIES
+  FOLDER Python
+)
 
 endfunction()
