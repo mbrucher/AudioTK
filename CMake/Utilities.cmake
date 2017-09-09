2f36abdce4eaa1a79bbc1f02b2ac5e7f764f1172
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-09-09 20:44:36 +0100

    Changed all the main cmake files (need to change the Python ones as well)

---
+++ CMake/Utilities.cmake
@@ -0,0 +1,58 @@
+#
+# CMake utilities for ATL
+#
+
+function(ATK_add_library PREFIX)
+
+SOURCE_GROUP_BY_FOLDER(${PREFIX})
+
+include_directories(${PROJECT_SOURCE_DIR})
+
+if(ENABLE_STATIC_LIBRARIES)
+  add_library(${PREFIX}_static
+    STATIC
+    ${${PREFIX}_SRC} ${${PREFIX}_HEADERS} ${NATVIS_FILE}
+  )
+
+  set_target_properties (${${PREFIX}_NAME}_static PROPERTIES
+    FOLDER C++/static
+  )
+
+  INSTALL(TARGETS ${${PREFIX}_NAME}_static
+    RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}/bin/ COMPONENT libraries
+    LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/ COMPONENT libraries
+    ARCHIVE DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/ COMPONENT libraries
+  )
+endif(ENABLE_STATIC_LIBRARIES)
+
+if(ENABLE_SHARED_LIBRARIES)
+  add_definitions(-DBUILD_${PREFIX})
+  add_definitions(-DATK_SHARED)
+
+  add_library(${${PREFIX}_NAME}
+    SHARED
+    ${ATK_UTILITY_SRC} ${ATK_UTILITY_HEADERS} ${NATVIS_FILE}
+  )
+
+  set_target_properties (${${PREFIX}_NAME} PROPERTIES
+    FOLDER C++/shared
+  )
+
+  target_link_libraries(${${PREFIX}_NAME} ${${PREFIX}_LIBRARIES})
+
+  stagedebug(${${PREFIX}_NAME})
+
+  INSTALL(TARGETS ${${PREFIX}_NAME}
+    RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}/bin/ COMPONENT libraries
+    LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/ COMPONENT libraries
+    ARCHIVE DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/ COMPONENT libraries
+  )
+
+endif(ENABLE_SHARED_LIBRARIES)
+
+INSTALL(FILES ${${PREFIX}_HEADERS}
+  DESTINATION ${CMAKE_INSTALL_PREFIX}/include/ATK/${${PREFIX}_FOLDER}
+)
+
+
+endfunction()
