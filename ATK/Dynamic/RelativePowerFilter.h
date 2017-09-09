81d671d1dee6dc80511b758762004987c8d1456a
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-09-09 17:14:51 +0100

    Fix some wrong narrowing (clang warnings)

--- ATK/Dynamic/RelativePowerFilter.h
+++ ATK/Dynamic/RelativePowerFilter.h
@@ -29,7 +29,7 @@ namespace ATK
     * @brief Constructor
     * @param nb_channels is the number of input and output channels
     */
-    RelativePowerFilter(int nb_channels = 1);
+    RelativePowerFilter(std::size_t nb_channels = 1);
     /// Destructor
     ~RelativePowerFilter();
 
