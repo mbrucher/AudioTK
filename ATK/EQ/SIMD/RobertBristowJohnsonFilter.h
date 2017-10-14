33b09ab2102931eb302a731182148bcb70ef9d7c
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-10-14 12:53:35 +0100

    RBJ vectorized filters

---
+++ ATK/EQ/SIMD/RobertBristowJohnsonFilter.h
@@ -0,0 +1,51 @@
+/**
+ * \file RobertBristowJohnsonFilter.h
+ */
+
+#ifndef ATK_EQ_SIMD_ROBERTBRISTOWJOHNSONFILTER_H
+#define ATK_EQ_SIMD_ROBERTBRISTOWJOHNSONFILTER_H
+
+#include <ATK/Core/BaseFilter.h>
+#include <ATK/EQ/config.h>
+
+namespace ATK
+{
+  /// Create a vectorized low pass Robert Bristow Johnson filter
+  template<typename DataType, std::size_t VL>
+  ATK_EQ_EXPORT std::unique_ptr<BaseFilter> createLowPassRobertBristowJohnsonFilter(std::size_t nb_channels = 1);
+  /// Create a vectorized high pass Robert Bristow Johnson filter
+  template<typename DataType, std::size_t VL>
+  ATK_EQ_EXPORT std::unique_ptr<BaseFilter> createHighPassRobertBristowJohnsonFilter(std::size_t nb_channels = 1);
+  /// Create a vectorized band pass Robert Bristow Johnson filter
+  template<typename DataType, std::size_t VL>
+  ATK_EQ_EXPORT std::unique_ptr<BaseFilter> createBandPassRobertBristowJohnsonFilter(std::size_t nb_channels = 1);
+  /// Create a vectorized band pass Robert Bristow Johnson filter
+  template<typename DataType, std::size_t VL>
+  ATK_EQ_EXPORT std::unique_ptr<BaseFilter> createBandPass2RobertBristowJohnsonFilter(std::size_t nb_channels = 1);
+  /// Create a vectorized band stop Robert Bristow Johnson filter
+  template<typename DataType, std::size_t VL>
+  ATK_EQ_EXPORT std::unique_ptr<BaseFilter> createBandStopRobertBristowJohnsonFilter(std::size_t nb_channels = 1);
+  /// Create a vectorized all pass Robert Bristow Johnson filter
+  template<typename DataType, std::size_t VL>
+  ATK_EQ_EXPORT std::unique_ptr<BaseFilter> createAllPassRobertBristowJohnsonFilter(std::size_t nb_channels = 1);
+  /// Create a vectorized low pass Robert Bristow Johnson TDF2 filter
+  template<typename DataType, std::size_t VL>
+  ATK_EQ_EXPORT std::unique_ptr<BaseFilter> createLowPassRobertBristowJohnsonTDF2Filter(std::size_t nb_channels = 1);
+  /// Create a vectorized high pass Robert Bristow Johnson TDF2 filter
+  template<typename DataType, std::size_t VL>
+  ATK_EQ_EXPORT std::unique_ptr<BaseFilter> createHighPassRobertBristowJohnsonTDF2Filter(std::size_t nb_channels = 1);
+  /// Create a vectorized band pass Robert Bristow Johnson TDF2 filter
+  template<typename DataType, std::size_t VL>
+  ATK_EQ_EXPORT std::unique_ptr<BaseFilter> createBandPassRobertBristowJohnsonTDF2Filter(std::size_t nb_channels = 1);
+  /// Create a vectorized band pass Robert Bristow Johnson TDF2 filter
+  template<typename DataType, std::size_t VL>
+  ATK_EQ_EXPORT std::unique_ptr<BaseFilter> createBandPass2RobertBristowJohnsonTDF2Filter(std::size_t nb_channels = 1);
+  /// Create a vectorized band stop Robert Bristow Johnson TDF2 filter
+  template<typename DataType, std::size_t VL>
+  ATK_EQ_EXPORT std::unique_ptr<BaseFilter> createBandStopRobertBristowJohnsonTDF2Filter(std::size_t nb_channels = 1);
+  /// Create a vectorized all pass Robert Bristow Johnson TDF2 filter
+  template<typename DataType, std::size_t VL>
+  ATK_EQ_EXPORT std::unique_ptr<BaseFilter> createAllPassRobertBristowJohnsonTDF2Filter(std::size_t nb_channels = 1);
+}
+
+#endif
