b1c98482531430ec5aec80c92e0086fd29472e37
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-10-28 17:48:49 +0100

    Proper export

--- ATK/Tools/SIMD/OneMinusFilter.cpp
+++ ATK/Tools/SIMD/OneMinusFilter.cpp
@@ -42,9 +42,9 @@ namespace ATK
                          ((std::size_t) nb_channels))
 
   SIMDPP_INSTANTIATE_DISPATCHER(
-    (template std::unique_ptr<BaseFilter> createOneMinusFilter<double, 2>(std::size_t)),
-    (template std::unique_ptr<BaseFilter> createOneMinusFilter<float, 4>(std::size_t)),
-    (template std::unique_ptr<BaseFilter> createOneMinusFilter<double, 4>(std::size_t)),
-    (template std::unique_ptr<BaseFilter> createOneMinusFilter<float, 8>(std::size_t)),
-    (template std::unique_ptr<BaseFilter> createOneMinusFilter<double, 8>(std::size_t)));
+    (template ATK_TOOLS_EXPORT std::unique_ptr<BaseFilter> createOneMinusFilter<double, 2>(std::size_t)),
+    (template ATK_TOOLS_EXPORT std::unique_ptr<BaseFilter> createOneMinusFilter<float, 4>(std::size_t)),
+    (template ATK_TOOLS_EXPORT std::unique_ptr<BaseFilter> createOneMinusFilter<double, 4>(std::size_t)),
+    (template ATK_TOOLS_EXPORT std::unique_ptr<BaseFilter> createOneMinusFilter<float, 8>(std::size_t)),
+    (template ATK_TOOLS_EXPORT std::unique_ptr<BaseFilter> createOneMinusFilter<double, 8>(std::size_t)));
 }
