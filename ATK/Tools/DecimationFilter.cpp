d6205ec3aa43254b9adf75f4dcb945288a4656f5
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-10-28 19:08:14 +0100

    Also preparing decimation filters for future SIMD

--- ATK/Tools/DecimationFilter.cpp
+++ ATK/Tools/DecimationFilter.cpp
@@ -2,55 +2,13 @@
  * \file DecimationFilter.cpp
  */
 
-#include <ATK/Tools/DecimationFilter.h>
+#include <ATK/Tools/DecimationFilter.hxx>
 
-#include <cassert>
 #include <complex>
 #include <cstdint>
 
 namespace ATK
 {
-  template<class DataType>
-  DecimationFilter<DataType>::DecimationFilter(std::size_t nb_channels)
-  :TypedBaseFilter<DataType>(nb_channels, nb_channels)
-  {
-  }
-  
-  template<class DataType>
-  DecimationFilter<DataType>::~DecimationFilter()
-  {
-  }
-  
-  template<class DataType>
-  void DecimationFilter<DataType>::setup()
-  {
-    Parent::setup();
-    if(output_sampling_rate == 0 || input_sampling_rate % output_sampling_rate != 0)
-    {
-      decimation = 0;
-    }
-    else
-    {
-      decimation = static_cast<int>(input_sampling_rate / output_sampling_rate);
-    }
-  }
-  
-  template<class DataType>
-  void DecimationFilter<DataType>::process_impl(std::size_t size) const
-  {
-    assert(nb_input_ports == nb_output_ports);
-
-    for(unsigned int channel = 0; channel < nb_input_ports; ++channel)
-    {
-      const DataType* ATK_RESTRICT input = converted_inputs[channel];
-      DataType* ATK_RESTRICT output = outputs[channel];
-      for(std::size_t i = 0; i < size; ++i)
-      {
-        output[i] = input[i*decimation];
-      }
-    }
-  }
-  
   template class DecimationFilter<std::int16_t>;
   template class DecimationFilter<std::int32_t>;
   template class DecimationFilter<int64_t>;
---
+++ ATK/Tools/DecimationFilter.hxx
@@ -0,0 +1,51 @@
+/**
+ * \file DecimationFilter.hxx
+ */
+
+#include <ATK/Tools/DecimationFilter.h>
+
+#include <cassert>
+
+namespace ATK
+{
+  template<class DataType>
+  DecimationFilter<DataType>::DecimationFilter(std::size_t nb_channels)
+  :TypedBaseFilter<DataType>(nb_channels, nb_channels)
+  {
+  }
+  
+  template<class DataType>
+  DecimationFilter<DataType>::~DecimationFilter()
+  {
+  }
+  
+  template<class DataType>
+  void DecimationFilter<DataType>::setup()
+  {
+    Parent::setup();
+    if(output_sampling_rate == 0 || input_sampling_rate % output_sampling_rate != 0)
+    {
+      decimation = 0;
+    }
+    else
+    {
+      decimation = static_cast<int>(input_sampling_rate / output_sampling_rate);
+    }
+  }
+  
+  template<class DataType>
+  void DecimationFilter<DataType>::process_impl(std::size_t size) const
+  {
+    assert(nb_input_ports == nb_output_ports);
+
+    for(unsigned int channel = 0; channel < nb_input_ports; ++channel)
+    {
+      const DataType* ATK_RESTRICT input = converted_inputs[channel];
+      DataType* ATK_RESTRICT output = outputs[channel];
+      for(std::size_t i = 0; i < size; ++i)
+      {
+        output[i] = input[i*decimation];
+      }
+    }
+  }
+}
