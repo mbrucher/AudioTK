fcbaa5208d7d1ff0c767969b600d07cfe6008510
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-11-11 21:59:38 +0000

    Reduce SIMD filter

---
+++ ATK/Tools/SIMD/ReduceFilter.cpp
@@ -0,0 +1,49 @@
+/**
+ * \file ReduceFilter.cpp
+ */
+
+#include <ATK/Tools/SIMD/ReduceFilter.hxx>
+#include <ATK/Core/TypeTraits.h>
+
+#include <simdpp/dispatch/dispatcher.h>
+#include <simdpp/dispatch/get_arch_gcc_builtin_cpu_supports.h>
+#include <simdpp/dispatch/get_arch_raw_cpuid.h>
+#include <simdpp/dispatch/get_arch_linux_cpuinfo.h>
+
+#if SIMDPP_HAS_GET_ARCH_RAW_CPUID
+# define SIMDPP_USER_ARCH_INFO ::simdpp::get_arch_raw_cpuid()
+#elif SIMDPP_HAS_GET_ARCH_GCC_BUILTIN_CPU_SUPPORTS
+# define SIMDPP_USER_ARCH_INFO ::simdpp::get_arch_gcc_builtin_cpu_supports()
+#elif SIMDPP_HAS_GET_ARCH_LINUX_CPUINFO
+# define SIMDPP_USER_ARCH_INFO ::simdpp::get_arch_linux_cpuinfo()
+#else
+# error "Unsupported platform"
+#endif
+
+namespace ATK
+{
+  template class ReduceFilter<simdpp::float64<2>, double>;
+  template class ReduceFilter<simdpp::float32<4>, float>;
+  template class ReduceFilter<simdpp::float64<4>, double>;
+  template class ReduceFilter<simdpp::float32<8>, float>;
+  template class ReduceFilter<simdpp::float64<8>, double>;
+  
+  namespace SIMDPP_ARCH_NAMESPACE
+  {
+  template<typename DataType, std::size_t VL>
+  std::unique_ptr<BaseFilter> createReduceFilter(std::size_t nb_channels)
+  {
+  return std::unique_ptr<BaseFilter>(new ReduceFilter<typename SIMDTypeTraits<DataType>::template SIMDType<VL>, DataType>(nb_channels));
+  }
+  }
+  
+  SIMDPP_MAKE_DISPATCHER((template<typename DataType, std::size_t VL>) (<DataType, VL>) (std::unique_ptr<BaseFilter>) (createReduceFilter)
+                         ((std::size_t) nb_channels))
+  
+  SIMDPP_INSTANTIATE_DISPATCHER(
+    (template ATK_TOOLS_EXPORT std::unique_ptr<BaseFilter> createReduceFilter<double, 2>(std::size_t)),
+    (template ATK_TOOLS_EXPORT std::unique_ptr<BaseFilter> createReduceFilter<float, 4>(std::size_t)),
+    (template ATK_TOOLS_EXPORT std::unique_ptr<BaseFilter> createReduceFilter<double, 4>(std::size_t)),
+    (template ATK_TOOLS_EXPORT std::unique_ptr<BaseFilter> createReduceFilter<float, 8>(std::size_t)),
+    (template ATK_TOOLS_EXPORT std::unique_ptr<BaseFilter> createReduceFilter<double, 8>(std::size_t)));
+}
