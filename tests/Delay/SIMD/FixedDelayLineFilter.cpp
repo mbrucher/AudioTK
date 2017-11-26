3d94d6b447563fd37f15ec82ef06e71d065e7bff
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-11-26 15:32:17 +0000

    Fix the test

--- tests/Delay/SIMD/FixedDelayLineFilter.cpp
+++ tests/Delay/SIMD/FixedDelayLineFilter.cpp
@@ -11,6 +11,7 @@
 #include <ATK/Core/OutPointerFilter.h>
 
 #include <ATK/Tools/SIMD/BroadcastFilter.h>
+#include <ATK/Tools/SIMD/ReduceFilter.h>
 
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_NO_MAIN
@@ -35,32 +36,33 @@ BOOST_AUTO_TEST_CASE(BOOST_PP_CAT(SIMDPP_ARCH_NAMESPACE, FixedDelayLineFilter_si
   broadcaster.set_input_sampling_rate(48000);
   broadcaster.set_input_port(0, &generator, 0);
   
-  std::array<simdpp::float32<4>, PROCESSSIZE> outdata;
   ATK::FixedDelayLineFilter<simdpp::float32<4>> filter(100);
   filter.set_input_sampling_rate(48000);
   filter.set_input_port(0, &broadcaster, 0);
   filter.set_delay(50);
 
-  ATK::OutPointerFilter<simdpp::float32<4>> output(outdata.data(), 1, PROCESSSIZE, false);
+  ATK::ReduceFilter<simdpp::float32<4>, float> reducer;
+  reducer.set_input_sampling_rate(48000);
+  reducer.set_input_port(0, &filter, 0);
+
+  std::array<float, PROCESSSIZE> outdata;
+  ATK::OutPointerFilter<float> output(outdata.data(), 1, PROCESSSIZE, false);
   output.set_input_sampling_rate(48000);
-  output.set_input_port(0, &filter, 0);
+  output.set_input_port(0, &reducer, 0);
 
   output.process(1);
   output.process(49);
   output.process(51);
   output.process(PROCESSSIZE - 1 - 49 -51);
   
-  auto zero = ATK::TypeTraits<simdpp::float32<4>>::Zero();
   for(ptrdiff_t i = 0; i < 50; ++i)
   {
-    bool res = std::memcmp(&outdata[i], &zero, sizeof(simdpp::float32<4>)) == 0;
-    BOOST_CHECK(res);
+    BOOST_REQUIRE_EQUAL(0, outdata[i]);
   }
   
   for(ptrdiff_t i = 50; i < PROCESSSIZE; ++i)
   {
-    bool res = std::memcmp(&outdata[i], data.data() + i - 50, sizeof(simdpp::float32<4>)) == 0;
-    BOOST_CHECK(res);
+    BOOST_REQUIRE_EQUAL(data[i - 50] * 4, outdata[i]);
   }
 }
 
