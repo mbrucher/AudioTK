33f9937250743d33d3276f9a40425585cf95b5be
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-01-07 13:54:22 +0000

    Cover PanFilter entirely

--- tests/Tools/PanFilter.cpp
+++ tests/Tools/PanFilter.cpp
@@ -13,7 +13,33 @@
 #define BOOST_TEST_NO_MAIN
 #include <boost/test/unit_test.hpp>
 
-#define PROCESSSIZE (1024)
+const size_t PROCESSSIZE = 1024;
+
+BOOST_AUTO_TEST_CASE( PanFilter_pan_set_test )
+{
+  ATK::PanFilter<double> panfilter;
+  panfilter.set_pan(0.5);
+  BOOST_CHECK_EQUAL(panfilter.get_pan(), 0.5);
+}
+
+BOOST_AUTO_TEST_CASE( PanFilter_pan_range_test )
+{
+  ATK::PanFilter<double> panfilter;
+  BOOST_CHECK_THROW(panfilter.set_pan(-1.00001), std::out_of_range);
+}
+
+BOOST_AUTO_TEST_CASE( PanFilter_pan_range2_test )
+{
+  ATK::PanFilter<double> panfilter;
+  BOOST_CHECK_THROW(panfilter.set_pan(1.00001), std::out_of_range);
+}
+
+BOOST_AUTO_TEST_CASE( PanFilter_panlaw_set_test )
+{
+  ATK::PanFilter<double> panfilter;
+  panfilter.set_pan_law(ATK::PanFilter<double>::PAN_LAWS::BALANCE);
+  BOOST_CHECK(panfilter.get_pan_law() == ATK::PanFilter<double>::PAN_LAWS::BALANCE);
+}
 
 BOOST_AUTO_TEST_CASE( PanFilter_center_sincos_0_left_test )
 {
