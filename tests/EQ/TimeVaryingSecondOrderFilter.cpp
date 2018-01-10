16adc5e795ea10279b60e4fe6d3bb008c9f38978
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-01-10 20:27:55 +0000

    More TV tests

--- tests/EQ/TimeVaryingSecondOrderFilter.cpp
+++ tests/EQ/TimeVaryingSecondOrderFilter.cpp
@@ -930,6 +930,131 @@ BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingLowShelvingCoefficients_20
   checker.process(PROCESSSIZE);
 }
 
+
+BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingLowShelvingCoefficients2_1k_test )
+{
+  ATK::SimpleSinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(1000);
+  
+  ATK::TimeVaryingIIRFilter<ATK::TimeVaryingLowShelvingCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_gain(2);
+  filter.set_min_frequency(100);
+  filter.set_max_frequency(12000);
+  filter.set_number_of_steps(12000 - 100 + 1);
+  
+  std::array<float, 2*PROCESSSIZE> data;
+  for(ptrdiff_t i = 0; i < 2*PROCESSSIZE; ++i)
+  {
+    data[i] = 100;
+  }
+  
+  ATK::InPointerFilter<float> generator2(data.data(), 1, 2*PROCESSSIZE, false);
+  generator2.set_output_sampling_rate(1024*64);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0.9999157109896207));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  filter.set_input_port(1, &generator2, 0);
+  
+  filter.process(1024*64);
+  filter.set_input_port(1, &generator2, 0);
+  
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingLowShelvingCoefficients2_100_test )
+{
+  ATK::SimpleSinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(100);
+  
+  ATK::TimeVaryingIIRFilter<ATK::TimeVaryingLowShelvingCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_gain(2);
+  filter.set_min_frequency(100);
+  filter.set_max_frequency(12000);
+  filter.set_number_of_steps(12000 - 100 + 1);
+  
+  std::array<float, 2*PROCESSSIZE> data;
+  for(ptrdiff_t i = 0; i < 2*PROCESSSIZE; ++i)
+  {
+    data[i] = 100;
+  }
+  
+  ATK::InPointerFilter<float> generator2(data.data(), 1, 2*PROCESSSIZE, false);
+  generator2.set_output_sampling_rate(1024*64);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(10, 0));
+  frequency_checks.push_back(std::make_pair(100, 1.257433429682748));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  filter.set_input_port(1, &generator2, 0);
+  
+  filter.process(1024*64);
+  
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingLowShelvingCoefficients2_200_test )
+{
+  ATK::SimpleSinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(200);
+  
+  ATK::TimeVaryingIIRFilter<ATK::TimeVaryingLowShelvingCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_gain(2);
+  filter.set_min_frequency(100);
+  filter.set_max_frequency(12000);
+  filter.set_number_of_steps(12000 - 100 + 1);
+  
+  std::array<float, 2*PROCESSSIZE> data;
+  for(ptrdiff_t i = 0; i < 2*PROCESSSIZE; ++i)
+  {
+    data[i] = 100;
+  }
+  
+  ATK::InPointerFilter<float> generator2(data.data(), 1, 2*PROCESSSIZE, false);
+  generator2.set_output_sampling_rate(1024*64);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(200, 1.0414630341965894));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  filter.set_input_port(1, &generator2, 0);
+  
+  filter.process(1024*64);
+  
+  checker.process(PROCESSSIZE);
+}
+
 BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingHighShelvingCoefficients_1k_test )
 {
   ATK::SimpleSinusGeneratorFilter<double> generator;
@@ -944,13 +1069,13 @@ BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingHighShelvingCoefficients_1
   filter.set_min_frequency(100);
   filter.set_max_frequency(12000);
   filter.set_number_of_steps(12000 - 100 + 1);
-
+  
   std::array<float, 2*PROCESSSIZE> data;
   for(ptrdiff_t i = 0; i < 2*PROCESSSIZE; ++i)
   {
     data[i] = 1000;
   }
-
+  
   ATK::InPointerFilter<float> generator2(data.data(), 1, 2*PROCESSSIZE, false);
   generator2.set_output_sampling_rate(1024*64);
   
@@ -965,7 +1090,7 @@ BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingHighShelvingCoefficients_1
   checker.set_input_port(0, &filter, 0);
   filter.set_input_port(0, &generator, 0);
   filter.set_input_port(1, &generator2, 0);
-
+  
   filter.process(1024*64);
   
   checker.process(PROCESSSIZE);
@@ -985,13 +1110,13 @@ BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingHighShelvingCoefficients_1
   filter.set_min_frequency(100);
   filter.set_max_frequency(12000);
   filter.set_number_of_steps(12000 - 100 + 1);
-
+  
   std::array<float, 2*PROCESSSIZE> data;
   for(ptrdiff_t i = 0; i < 2*PROCESSSIZE; ++i)
   {
     data[i] = 1000;
   }
-
+  
   ATK::InPointerFilter<float> generator2(data.data(), 1, 2*PROCESSSIZE, false);
   generator2.set_output_sampling_rate(1024*64);
   
@@ -1006,7 +1131,7 @@ BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingHighShelvingCoefficients_1
   checker.set_input_port(0, &filter, 0);
   filter.set_input_port(0, &generator, 0);
   filter.set_input_port(1, &generator2, 0);
-
+  
   filter.process(1024*64);
   
   checker.process(PROCESSSIZE);
@@ -1026,6 +1151,129 @@ BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingHighShelvingCoefficients_5
   filter.set_min_frequency(100);
   filter.set_max_frequency(12000);
   filter.set_number_of_steps(12000 - 100 + 1);
+  
+  std::array<float, 2*PROCESSSIZE> data;
+  for(ptrdiff_t i = 0; i < 2*PROCESSSIZE; ++i)
+  {
+    data[i] = 1000;
+  }
+  
+  ATK::InPointerFilter<float> generator2(data.data(), 1, 2*PROCESSSIZE, false);
+  generator2.set_output_sampling_rate(1024*64);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  frequency_checks.push_back(std::make_pair(500, 0.960256352408842));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  filter.set_input_port(1, &generator2, 0);
+  
+  filter.process(1024*64);
+  
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingHighShelvingCoefficients2_1k_test )
+{
+  ATK::SimpleSinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(1000);
+  
+  ATK::TimeVaryingIIRFilter<ATK::TimeVaryingHighShelvingCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_gain(2);
+  filter.set_min_frequency(100);
+  filter.set_max_frequency(12000);
+  filter.set_number_of_steps(12000 - 100 + 1);
+
+  std::array<float, 2*PROCESSSIZE> data;
+  for(ptrdiff_t i = 0; i < 2*PROCESSSIZE; ++i)
+  {
+    data[i] = 1000;
+  }
+
+  ATK::InPointerFilter<float> generator2(data.data(), 1, 2*PROCESSSIZE, false);
+  generator2.set_output_sampling_rate(1024*64);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 1.257433429682748));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  filter.set_input_port(1, &generator2, 0);
+
+  filter.process(1024*64);
+  
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingHighShelvingCoefficients2_10k_test )
+{
+  ATK::SimpleSinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(10000);
+  
+  ATK::TimeVaryingIIRFilter<ATK::TimeVaryingHighShelvingCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_gain(2);
+  filter.set_min_frequency(100);
+  filter.set_max_frequency(12000);
+  filter.set_number_of_steps(12000 - 100 + 1);
+
+  std::array<float, 2*PROCESSSIZE> data;
+  for(ptrdiff_t i = 0; i < 2*PROCESSSIZE; ++i)
+  {
+    data[i] = 1000;
+  }
+
+  ATK::InPointerFilter<float> generator2(data.data(), 1, 2*PROCESSSIZE, false);
+  generator2.set_output_sampling_rate(1024*64);
+  
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(10000, 1.4142135623730951));
+  frequency_checks.push_back(std::make_pair(1000, 0));
+  checker.set_checks(frequency_checks);
+  
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+  filter.set_input_port(1, &generator2, 0);
+
+  filter.process(1024*64);
+  
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingHighShelvingCoefficients2_500_test )
+{
+  ATK::SimpleSinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(500);
+  
+  ATK::TimeVaryingIIRFilter<ATK::TimeVaryingHighShelvingCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_gain(2);
+  filter.set_min_frequency(100);
+  filter.set_max_frequency(12000);
+  filter.set_number_of_steps(12000 - 100 + 1);
 
   std::array<float, 2*PROCESSSIZE> data;
   for(ptrdiff_t i = 0; i < 2*PROCESSSIZE; ++i)
@@ -1041,7 +1289,7 @@ BOOST_AUTO_TEST_CASE( TimeVaryingIIRFilter_TimeVaryingHighShelvingCoefficients_5
   std::vector<std::pair<int, double> > frequency_checks;
   frequency_checks.push_back(std::make_pair(100, 0));
   frequency_checks.push_back(std::make_pair(1000, 0));
-  frequency_checks.push_back(std::make_pair(500, 0.960256352408842));
+  frequency_checks.push_back(std::make_pair(500, 1.0414630341965894));
   checker.set_checks(frequency_checks);
   
   checker.set_input_port(0, &filter, 0);
