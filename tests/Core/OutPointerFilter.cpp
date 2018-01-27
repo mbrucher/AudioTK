28f828212db2707acb6165f5e3e82b9cb8d3c160
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-01-27 19:41:10 +0000

    Adding a bypass for the out pointer filter

--- tests/Core/OutPointerFilter.cpp
+++ tests/Core/OutPointerFilter.cpp
@@ -122,6 +122,7 @@ BOOST_AUTO_TEST_CASE( OutPointerFloat_sin1k2k_interleaved_test )
   
   output.process(2);
   output.process(PROCESSSIZE - 2);
+  output.process(PROCESSSIZE); // noop
   
   for(ptrdiff_t i = 0; i < 2*PROCESSSIZE; ++i)
   {
