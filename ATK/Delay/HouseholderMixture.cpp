991455860e704c1590495dd9ea50de3c8b5d1521
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-02-04 12:49:13 +0000

    Split the delays

--- ATK/Delay/HouseholderMixture.cpp
+++ ATK/Delay/HouseholderMixture.cpp
@@ -2,41 +2,12 @@
  * \file HouseholderMixture.cpp
  */
 
-#include <ATK/Delay/FeedbackDelayNetworkFilter.hxx>
-#include <ATK/Delay/HouseholderMixture.h>
+#include <ATK/Delay/HouseholderMixture.hxx>
 
 #include <complex>
 
-#include <Eigen/Dense>
-
 namespace ATK
 {
-  template<typename DataType_, unsigned int nb_channels>
-  class HouseholderMixture<DataType_, nb_channels>::MixtureImpl
-  {
-  public:
-    typedef Eigen::Matrix<DataType, nb_channels, 1> Vector;
-    typedef Eigen::Matrix<DataType, nb_channels, nb_channels> Matrix;
-
-    MixtureImpl()
-      :transition(create())
-    {
-    }
-
-    Vector mix(const Vector& x) const
-    {
-      return transition * x;
-    }
-
-  protected:
-    const Matrix transition;
-    
-    static Matrix create()
-    {
-      return Matrix::Identity().array() - (DataType(2) / DataType(nb_channels));
-    }
-  };
-
   template class FeedbackDelayNetworkFilter<HouseholderMixture<float, 2>>;
   template class FeedbackDelayNetworkFilter<HouseholderMixture<double, 2>>;
   template class FeedbackDelayNetworkFilter<HouseholderMixture<std::complex<float>, 2>>;
