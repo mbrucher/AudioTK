ce80fc92e87f1d1db56ce14eaea88bff178d3bfa
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-03-04 19:40:55 +0000

    Adding a FTZ test that would just start and stop.

---
+++ tests/Utility/FlushToZero.cpp
@@ -0,0 +1,16 @@
+/**
+ * \ file FlushToZero.cpp
+ */
+
+#include <cmath>
+
+#include <ATK/Utility/FlushToZero.h>
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_NO_MAIN
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(FTZ_test)
+{
+  ATK::FlushToZero ftz;
+}
