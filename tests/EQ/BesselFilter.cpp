c22c5cf20a62355dcd7fed9ec55275844807e854
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-09-30 20:58:48 +0100

    Remove float test

--- tests/EQ/BesselFilter.cpp
+++ tests/EQ/BesselFilter.cpp
@@ -173,36 +173,7 @@ filter.set_input_port(0, &generator, 0);\
 filter.process(1024*64);\
 \
 checker.process(PROCESSSIZE);\
-}\
-\
-BOOST_AUTO_TEST_CASE( IIRFilter_BesselHighPassCoefficients_##i##_1k_test )\
-{\
-  ATK::SimpleSinusGeneratorFilter<double> generator;\
-  generator.set_output_sampling_rate(1024*64);\
-  generator.set_amplitude(1);\
-  generator.set_frequency(1000);\
-\
-  ATK::IIRFilter<ATK::BesselHighPassCoefficients<type> > filter;\
-  filter.set_input_sampling_rate(1024*64);\
-  filter.set_output_sampling_rate(1024*64);\
-  filter.set_cut_frequency(100);\
-  filter.set_order(3);\
-\
-  ATK::FFTCheckerFilter<double> checker;\
-  checker.set_input_sampling_rate(1024*64);\
-  std::vector<std::pair<int, double> > frequency_checks;\
-  frequency_checks.push_back(std::make_pair(100, 0));\
-  frequency_checks.push_back(std::make_pair(1000, 0.9969240497367295));\
-  frequency_checks.push_back(std::make_pair(10000, 0));\
-  checker.set_checks(frequency_checks);\
-\
-  checker.set_input_port(0, &filter, 0);\
-  filter.set_input_port(0, &generator, 0);\
-\
-  filter.process(1024*64);\
-\
-  checker.process(PROCESSSIZE);\
-}\
+}
 
 check_bessel(0, float)
 check_bessel(1, double)
@@ -349,6 +320,35 @@ BOOST_AUTO_TEST_CASE( IIRFilter_BesselLowPassCoefficients_200_test )
   
   filter.process(1024*64);
   
+  checker.process(PROCESSSIZE);
+}
+
+BOOST_AUTO_TEST_CASE( IIRFilter_BesselHighPassCoefficients_1k_test )
+{
+  ATK::SimpleSinusGeneratorFilter<double> generator;
+  generator.set_output_sampling_rate(1024*64);
+  generator.set_amplitude(1);
+  generator.set_frequency(1000);
+
+  ATK::IIRFilter<ATK::BesselHighPassCoefficients<double> > filter;
+  filter.set_input_sampling_rate(1024*64);
+  filter.set_output_sampling_rate(1024*64);
+  filter.set_cut_frequency(100);
+  filter.set_order(3);
+
+  ATK::FFTCheckerFilter<double> checker;
+  checker.set_input_sampling_rate(1024*64);
+  std::vector<std::pair<int, double> > frequency_checks;
+  frequency_checks.push_back(std::make_pair(100, 0));
+  frequency_checks.push_back(std::make_pair(1000, 0.9969240497367295));
+  frequency_checks.push_back(std::make_pair(10000, 0));
+  checker.set_checks(frequency_checks);
+
+  checker.set_input_port(0, &filter, 0);
+  filter.set_input_port(0, &generator, 0);
+
+  filter.process(1024*64);
+
   checker.process(PROCESSSIZE);
 }
 
